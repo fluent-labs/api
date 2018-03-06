fc53d1d2266bf25108f42a4cbb8d314342a7e707
Lucas Kjaero <lucas@lucaskjaero.com>
2018-03-05 22:47:19 -0800

    Add views

--- ForeignLanguageReader/urls.py
+++ ForeignLanguageReader/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .SubmitPage.views import submit
 
 urlpatterns = [
+    url(r'^$', submit),
     path('admin/', admin.site.urls),
 ]
--- SubmitPage/views.py
+++ SubmitPage/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
 # Create your views here.
+def submit(request):
+    template = loader.get_template('SubmitPage/submit.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
