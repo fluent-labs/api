97cbc9259ae377380b59b4e56176527af932dbf3
Lucas Kjaero <lucas@lucaskjaero.com>
2018-05-01 08:02:27 -0700

    First ajax call

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({text: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const postBody = this.state.text;
+    console.log(postBody);
+
+    axios
+      .post("http://localhost:8000/v1/chinese/document", {
+      	"text": postBody
+      })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => console.log(error));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="form-group">
+          <form onSubmit={this.handleSubmit}>
+            <label>Enter some new text here:</label><br></br>
+            <textarea className="form-control" rows="5" id="new-text" value={this.state.text} onChange={this.handleChange}></textarea><br></br>
+            <input type="submit" value="Submit"></input>
+          </form>
+        </div>
       </div>
     );
   }
