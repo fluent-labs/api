4216750a08ef0aa737234f4f8910d8cec7d4a195
Lucas Kjaero <lucas@lucaskjaero.com>
2020-01-13 18:31:59 -0800

    Fix lint errors in components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
-import Definition from './Definition';
+import React, { Component } from "react";
+import "./App.css";
+import Definition from "./Definition";
 
 import axios from "axios";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
+      language: "chinese",
       statusText: "Submit to get definitions",
-      text: '',
+      text: "",
       words: []
     };
 
@@ -20,57 +20,72 @@ class App extends Component {
 
   // Needed to keep data in the field
   handleChange(event) {
-    this.setState({text: event.target.value});
+    this.setState({ text: event.target.value });
   }
 
   handleSubmit(event) {
     event.preventDefault();
 
-    this.setState({statusText: "Loading..."});
-
-    const language = this.refs.Language.value;
-    this.setState({language: language});
+    this.setState({ statusText: "Loading..." });
 
     const urlBase = "https://" + window.location.hostname + "/stemming/v1/";
-    const url = urlBase + language + "/document";
+    const url = urlBase + this.state.language + "/document";
 
     const postBody = this.state.text;
 
     axios
       .post(url, {
-      	"text": postBody
+        text: postBody
       })
       .then(response => {
         const responseBody = response.data;
 
         if (responseBody.status === "OK") {
-          this.setState({words: responseBody.words});
+          this.setState({ words: responseBody.words });
         }
       })
       .catch(error => {
-        this.setState({statusText: "Error loading words"});
-        console.log(error);
+        this.setState({ statusText: "Error loading words" });
       });
   }
 
   render() {
-    const statusText = this.state.words.length === 0 ? this.state.statusText : "Words in your text: ";
+    const statusText =
+      this.state.words.length === 0
+        ? this.state.statusText
+        : "Words in your text: ";
 
     return (
       <div className="App">
         <div className="form-group">
           <form onSubmit={this.handleSubmit}>
-            <label>Enter some new text here:</label><br></br>
-            <textarea className="form-control" rows="5" id="new-text" value={this.state.text} onChange={this.handleChange} ref="Text"></textarea><br></br>
-            <select ref="Language">
+            <label>Enter some new text here:</label>
+            <br />
+            <textarea
+              className="form-control"
+              rows="5"
+              id="new-text"
+              value={this.state.text}
+              onChange={this.handleChange}
+            ></textarea>
+            <br />
+            <select>
               <option value="chinese">Chinese</option>
             </select>
             <input type="submit" value="Submit"></input>
-          </form><br />
+          </form>
+          <br />
         </div>
         <div>
-          <b>{ statusText }</b><br />
-          { this.state.words.map(word => <Definition character={word} key={word} language={this.state.language} />) }
+          <b>{statusText}</b>
+          <br />
+          {this.state.words.map(word => (
+            <Definition
+              character={word}
+              key={word}
+              language={this.state.language}
+            />
+          ))}
         </div>
       </div>
     );
