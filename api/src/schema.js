f246a8e5edb38197fa8954a0175e0bcfdb680d5a
Lucas Kjaero <lucas@lucaskjaero.com>
2020-01-13 23:14:45 -0800

    Fix list nullabilities and add passthrough type

--- api/src/schema.js
+++ api/src/schema.js
@@ -2,15 +2,9 @@ const { gql } = require('apollo-server');
 
 const typeDefs = gql`
   type Query {
-    wordsInText: [Word]!
-    wordInformation(words: [String]!): [Word]!
+    wordsInText(text: String!): [Word!]!
+    wordInformation(words: [String!]!): [Word!]!
     me: User
-  }
-
-  type User {
-    id: ID!
-    email: String!
-    vocabulary: [Word]!
   }
 
   type Word {
@@ -25,6 +19,19 @@ const typeDefs = gql`
     CHINESE
     ENGLISH
     SPANISH
+  }
+
+  type Vocabulary {
+    id: ID!
+    user: User
+    word: Word!
+    added: String
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    vocabulary: [Vocabulary!]!
   }
 `;
 
