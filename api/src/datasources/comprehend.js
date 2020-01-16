452ab06a94287ea8ce8201883d05d2ab7aa22312
Lucas Kjaero <lucas@lucaskjaero.com>
2020-01-15 23:16:09 -0800

    Add aws call to comprehend api

--- api/src/datasources/comprehend.js
+++ api/src/datasources/comprehend.js
@@ -1,8 +1,24 @@
 const { RESTDataSource } = require("apollo-datasource-rest");
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "us-west-2" });
+
+const comprehend = new AWS.Comprehend();
 
 class AWSComprehendAPI extends RESTDataSource {
   constructor() {
     super();
+  }
+
+  getWordsInText({ text }) {
+    comprehend.detectSyntax({ LanguageCode: "en", Text: text }, (err, data) => {
+      if (err) {
+        console.log(err);
+        return [];
+      } else {
+        console.log(data);
+        return Array.isArray(data) ? data : [];
+      }
+    });
   }
 }
 
