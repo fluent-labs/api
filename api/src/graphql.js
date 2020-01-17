7b566340c6b51aa583f3162240f0297a2a8c3a98
Lucas Kjaero <lucas@lucaskjaero.com>
2020-01-16 23:11:20 -0800

    Create lambda handler

---
+++ api/src/graphql.js
@@ -0,0 +1,14 @@
+const { ApolloServer } = require('apollo-server-lambda');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+const GoogleNaturalLanguageAPI = require('./datasources/google');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    googleAPI: new GoogleNaturalLanguageAPI()
+  })
+});
+
+exports.graphqlHandler = server.createHandler();
