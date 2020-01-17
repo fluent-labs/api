69de4eb0eb67bda99cca570ac3938820ef19090e
Lucas Kjaero <lucas@lucaskjaero.com>
2020-01-16 23:09:38 -0800

    Fix errors

--- api/src/index.js
+++ api/src/index.js
@@ -1,14 +1,12 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
-const ComprehendAPI = require('./datasources/comprehend');
 const GoogleNaturalLanguageAPI = require('./datasources/google');
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   dataSources: () => ({
-    comprehendAPI: new ComprehendAPI(),
     googleAPI: new GoogleNaturalLanguageAPI()
   })
 });
