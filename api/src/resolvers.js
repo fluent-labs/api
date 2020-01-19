617e01f85a6a069b998204d9fb2241965bedc4a9
Lucas Kjaero-Zhang <lucaskjaero@users.noreply.github.com>
2020-01-18 22:05:08 -0800

    Chinese content (#10)
    * Words can have multiple definitions
    
    * Chinese content
    
    * Special fields for Chinese words
    
    * Resolve chinese word fields
    
    * Allow generic word type

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -1,10 +1,48 @@
+const chinese = require("../content/chinese/cedict.json").reduce((acc, word) => {
+  if (acc.hasOwnProperty(word.simplified)) {
+    acc[word.simplified].push(word);
+  } else {
+    acc[word.simplified] = [word];
+  }
+  return acc;
+}, {});
+
 module.exports = {
   Query: {
-    wordsInText: async (_, { text }, { dataSources }) => {
+    wordsInText: (_parent, { text }, { dataSources }) => {
       return dataSources.googleAPI.getWordsInText({ text: text });
     },
-    health: (_, _inputs, { dataSources }) => {
+    health: (_parent, _args, { dataSources }) => {
       return "OK";
+    }
+  },
+  Word: {
+    __resolveType: (word, _context, _info) => {
+      if (word.language == "CHINESE") return "ChineseWord";
+      else return GenericWord;
+    }
+  },
+  ChineseWord: {
+    definitions: (word, _args, _context) => {
+      if (chinese.hasOwnProperty(word.text)) {
+        return chinese[word.text].flatMap(word => word.definitions);
+      }
+    },
+    hsk: (word, _args, _context) => {
+      if (chinese.hasOwnProperty(word.text)) {
+        const levels = chinese[word.text]
+          .filter(word => word.hasOwnProperty("HSK"))
+          .map(word => word["HSK"]);
+
+        if (levels != []) {
+          return Math.min(levels);
+        }
+      }
+    },
+    pinyin: (word, _args, _context) => {
+      if (chinese.hasOwnProperty(word.text)) {
+        return chinese[word.text].map(word => word.pinyin);
+      }
     }
   }
 };
