92a9d370500cd1046981570cc3389aa4baaf4ad2
Lucas Kjaero <lucas@lucaskjaero.com>
2020-01-19 00:36:18 -0800

    Migrate to backend

--- api/deploy.tf
+++ api/deploy.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "foreign-language-reader"
+
+    workspaces {
+      name = "foreign-language-reader"
+    }
+  }
+}
+
 provider "aws" {
   profile    = "default"
   region     = "us-west-1"
