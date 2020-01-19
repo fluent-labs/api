f284a9e72c494c851083ee3d8b0f094b3cd42551
Lucas Kjaero <lucas@lucaskjaero.com>
2020-01-18 22:58:05 -0800

    Fixes

--- api/deploy.tf
+++ api/deploy.tf
@@ -6,8 +6,8 @@ provider "aws" {
 resource "aws_lambda_function" "foreign-language-reader-api" {
   function_name = "ForeignLanguageReaderAPI"
 
-  s3_bucket = "terraform-serverless-example"
-  s3_key    = "v1.0.0/example.zip"
+  s3_bucket = "foreign-language-reader-deploy"
+  s3_key    = "foreign-language-reader.zip"
 
   handler = "graphql.graphqlHandler"
   runtime = "nodejs12.x"
@@ -19,19 +19,19 @@ resource "aws_iam_role" "lambda_exec" {
    name = "foreign_language_reader_lambda"
 
    assume_role_policy = <<EOF
- {
-   "Version": "2012-10-17",
-   "Statement": [
-     {
-       "Action": "sts:AssumeRole",
-       "Principal": {
-         "Service": "lambda.amazonaws.com"
-       },
-       "Effect": "Allow",
-       "Sid": ""
-     }
-   ]
- }
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
  EOF
 
  }
