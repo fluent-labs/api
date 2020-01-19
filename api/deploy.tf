96ac2a9da8f8fabf6d5432d77d57703534a53ea5
Lucas Kjaero <lucas@lucaskjaero.com>
2020-01-19 00:05:26 -0800

    Keep adding the terraform

--- api/deploy.tf
+++ api/deploy.tf
@@ -2,21 +2,8 @@ provider "aws" {
   profile    = "default"
   region     = "us-west-1"
 }
-
-resource "aws_lambda_function" "foreign-language-reader-api" {
-  function_name = "ForeignLanguageReaderAPI"
-
-  s3_bucket = "foreign-language-reader-deploy"
-  s3_key    = "foreign-language-reader.zip"
-
-  handler = "graphql.graphqlHandler"
-  runtime = "nodejs12.x"
-
-  role = aws_iam_role.lambda_exec.arn
-}
-
 resource "aws_iam_role" "lambda_exec" {
-   name = "foreign_language_reader_lambda"
+   name = "foreign_language_reader_api"
 
    assume_role_policy = <<EOF
 {
@@ -34,4 +21,62 @@ resource "aws_iam_role" "lambda_exec" {
 }
  EOF
 
+ }
+
+resource "aws_lambda_function" "foreign-language-reader-api" {
+  function_name = "ForeignLanguageReaderAPI"
+
+  s3_bucket = "foreign-language-reader-deploy"
+  s3_key    = "foreign-language-reader.zip"
+
+  handler = "graphql.graphqlHandler"
+  runtime = "nodejs12.x"
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_api_gateway_rest_api" "rest_api" {
+  name        = "ForeignLanguageReaderAPI"
+  description = "Gateway for the foreign language reader API"
+}
+
+ resource "aws_api_gateway_resource" "proxy" {
+   rest_api_id = aws_api_gateway_rest_api.rest_api.id
+   parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
+   path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+   resource_id   = aws_api_gateway_resource.proxy.id
+   http_method   = "ANY"
+   authorization = "NONE"
+ }
+
+ resource "aws_api_gateway_integration" "lambda" {
+   rest_api_id = aws_api_gateway_rest_api.rest_api.id
+   resource_id = aws_api_gateway_method.proxy.resource_id
+   http_method = aws_api_gateway_method.proxy.http_method
+
+   integration_http_method = "POST"
+   type                    = "AWS_PROXY"
+   uri                     = aws_lambda_function.foreign-language-reader-api.invoke_arn
+ }
+
+ resource "aws_api_gateway_deployment" "deployment" {
+   depends_on = [
+     aws_api_gateway_integration.lambda,
+   ]
+
+   rest_api_id = aws_api_gateway_rest_api.rest_api.id
+   stage_name  = "test"
+ }
+
+  resource "aws_lambda_permission" "apigw" {
+   statement_id  = "AllowAPIGatewayInvoke"
+   action        = "lambda:InvokeFunction"
+   function_name = aws_lambda_function.foreign-language-reader-api.function_name
+   principal     = "apigateway.amazonaws.com"
+
+   source_arn = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/*"
  }
