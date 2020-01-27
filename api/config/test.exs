26351b4ea7f4383ec88bec2ac1c208f341409645
Lucas Kjaero <lucas@lucaskjaero.com>
2020-01-26 23:19:44 -0800

    Port should not be a string

--- api/config/test.exs
+++ api/config/test.exs
@@ -7,7 +7,7 @@ config :api, Api.Repo,
   password: "root",
   database: "api_test",
   hostname: "127.0.0.1",
-  port: "33061"
+  port: 33061,
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
