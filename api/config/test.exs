6883bd3c01fbc72612d2a9862ce12e7036ce7261
Lucas Kjaero <lucas@lucaskjaero.com>
2020-01-26 21:53:44 -0800

    Mysql doesn't work with localhost apparently

--- api/config/test.exs
+++ api/config/test.exs
@@ -6,7 +6,7 @@ config :api, Api.Repo,
   username: "root",
   password: "root",
   database: "api_test",
-  hostname: "localhost",
+  hostname: "127.0.0.1",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
