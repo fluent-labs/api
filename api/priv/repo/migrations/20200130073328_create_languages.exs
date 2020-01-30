7f8c988eb0115fedaff1f5f793f7431eb67bb267
Lucas Kjaero <lucas@lucaskjaero.com>
2020-01-29 23:34:54 -0800

    Language

---
+++ api/priv/repo/migrations/20200130073328_create_languages.exs
@@ -0,0 +1,13 @@
+defmodule Api.Repo.Migrations.CreateLanguages do
+  use Ecto.Migration
+
+  def change do
+    create table(:languages) do
+      add :name, :string
+
+      timestamps()
+    end
+
+    create unique_index(:languages, [:name])
+  end
+end
