fa4c29ef9e24dd2fa1f533535c97e71f015722f3
Lucas Kjaero <lucas@lucaskjaero.com>
2020-02-02 14:53:33 -0800

    Just run the formatter locally

--- vocabulary/deploy_lambda.tf
+++ vocabulary/deploy_lambda.tf
@@ -57,43 +57,43 @@ resource "aws_api_gateway_rest_api" "rest_api" {
   description = "Gateway for the foreign language reader vocabulary lambda"
 }
 
- resource "aws_api_gateway_resource" "proxy" {
-   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-   parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
-   path_part   = "{proxy+}"
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
+  path_part   = "{proxy+}"
 }
 
 resource "aws_api_gateway_method" "proxy" {
-   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
-   resource_id   = aws_api_gateway_resource.proxy.id
-   http_method   = "ANY"
-   authorization = "NONE"
- }
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
 
- resource "aws_api_gateway_integration" "lambda" {
-   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-   resource_id = aws_api_gateway_method.proxy.resource_id
-   http_method = aws_api_gateway_method.proxy.http_method
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_method.proxy.resource_id
+  http_method = aws_api_gateway_method.proxy.http_method
 
-   integration_http_method = "POST"
-   type                    = "AWS_PROXY"
-   uri                     = aws_lambda_function.foreign-language-reader-vocabulary-lambda.invoke_arn
- }
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.foreign-language-reader-vocabulary-lambda.invoke_arn
+}
 
- resource "aws_api_gateway_deployment" "deployment" {
-   depends_on = [
-     aws_api_gateway_integration.lambda,
-   ]
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [
+    aws_api_gateway_integration.lambda,
+  ]
 
-   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-   stage_name  = "test"
- }
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  stage_name  = "test"
+}
 
-  resource "aws_lambda_permission" "apigw" {
-   statement_id  = "AllowAPIGatewayInvoke"
-   action        = "lambda:InvokeFunction"
-   function_name = aws_lambda_function.foreign-language-reader-vocabulary-lambda.function_name
-   principal     = "apigateway.amazonaws.com"
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.foreign-language-reader-vocabulary-lambda.function_name
+  principal     = "apigateway.amazonaws.com"
 
-   source_arn = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/*"
- }
+  source_arn = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/*"
+}
