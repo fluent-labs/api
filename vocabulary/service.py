a1da5765e2cd51902278f8675a9eeb3729b0fda1
Lucas Kjaero-Zhang <lucaskjaero@users.noreply.github.com>
2020-02-02 11:16:04 -0800

    Add logging to the vocabulary lambda (#37)
    * Add logging to the vocabulary lambda
    
    * This can also fail
    
    * Simplify flow

--- vocabulary/service.py
+++ vocabulary/service.py
@@ -6,22 +6,42 @@ from wiktionaryparser import WiktionaryParser
 parser = WiktionaryParser()
 
 
-def get_vocabulary(word, language):
-    return parser.fetch(word, language)
+def return_error(message):
+    return {
+        "statusCode": 400,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps({"error": message})
+        }
 
 
 def handler(event, context):
-    print(event)
+    try:
+        body = json.loads(event.get("body"))
+    except Exception as e:
+        print(e)
+        return_error("Could not parse request body, is it valid?")
 
-    body = json.loads(event.get('body'))
+    if body is None or body == "":
+        return_error("You must post properties to this endpoint.")
+    elif "language" not in body:
+        return_error("You must specify a language.")
+    elif "word" not in body:
+        return_error("You must specify a word.")
 
-    language = body['language']
-    word = body['word']
-
+    language = body["language"]
+    word = body["word"]
     print("Getting vocabulary for language: %s and word: %s" % (language, word))
 
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type": "application/json"},
-        "body": json.dumps(get_vocabulary(word, language))
-        }
+    try:
+        return {
+            "statusCode": 200,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps(parser.fetch(word, language))
+            }
+    except Exception as e:
+        print(e)
+        return {
+            "statusCode": 500,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps({"error": "Error getting data from Wikimedia"})
+            }
