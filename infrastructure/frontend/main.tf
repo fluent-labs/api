389664f13279005b15f2eae0eaf3c314656279cd
Lucas Kjaero <lucas@lucaskjaero.com>
2020-02-04 22:54:58 -0800

    Make S3 bucket publically accessible

--- infrastructure/frontend/main.tf
+++ infrastructure/frontend/main.tf
@@ -7,3 +7,23 @@ resource "aws_s3_bucket" "foreign-language-reader-frontend" {
     error_document = "error.html"
   }
 }
+
+resource "aws_s3_bucket_policy" "public-access" {
+  bucket = aws_s3_bucket.foreign-language-reader-frontend.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "PolicyForPublicWebsiteContent",
+  "Statement": [
+    {
+      "Sid": "IPAllow",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.foreign-language-reader-frontend.arn}/*",
+    }
+  ]
+}
+POLICY
+}
