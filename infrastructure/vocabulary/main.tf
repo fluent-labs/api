29e72767cd3a15cad62f427beb4164393ba4e369
Lucas Kjaero <lucas@lucaskjaero.com>
2020-02-04 21:41:10 -0800

    It's always var

--- infrastructure/vocabulary/main.tf
+++ infrastructure/vocabulary/main.tf
@@ -71,7 +71,7 @@ resource "aws_api_gateway_rest_api" "rest_api" {
 resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
   parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
-  path_part   = "wiktionary-vocabulary-lookup-${env}"
+  path_part   = "wiktionary-vocabulary-lookup-${var.env}"
 }
 
 resource "aws_api_gateway_method" "proxy" {
