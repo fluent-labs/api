b5e6125d12d66b8f0af1a098dbbaa41679596e77
Lucas Kjaero <lucas@lucaskjaero.com>
2020-02-06 08:22:06 -0800

    Wellll

--- infrastructure/api/main.tf
+++ infrastructure/api/main.tf
@@ -100,8 +100,6 @@ resource "aws_db_instance" "default" {
   password                  = var.rds_password
   parameter_group_name      = "default.mysql5.7"
   skip_final_snapshot       = true
-  final_snapshot_identifier = "foreign-language-reader-${var.env}-final-snapshot"
-  snapshot_identifier       = "foreign-language-reader-${var.env}-snapshot"
   vpc_security_group_ids    = [aws_security_group.database.id]
   db_subnet_group_name      = aws_db_subnet_group.main.id
 }
