54fff348f5d9f549ca9f491f52120b2a61bbfdef
Lucas Kjaero-Zhang <lucaskjaero@users.noreply.github.com>
2020-02-05 22:23:40 -0800

    Network routing (#59)
    * Route tables for public subnet
    
    * Gateway for private subnet
    
    * Route tables for private network
    
    * Silence warnings
    
    * Run the formatter

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -87,6 +87,48 @@ resource "aws_network_acl_rule" "inbound_ssh_traffic" {
   to_port        = 22
 }
 
+# Route the public subnet traffic through the IGW
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_vpc.main.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_eip" "gw" {
+  count      = 2
+  vpc        = true
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_nat_gateway" "gw" {
+  count         = 2
+  subnet_id     = element(aws_subnet.public.*.id, count.index)
+  allocation_id = element(aws_eip.gw.*.id, count.index)
+}
+
+# Create a new route table for the private subnets
+# And make it route non-local traffic through the NAT gateway to the internet
+resource "aws_route_table" "private" {
+  count  = 2
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = element(aws_nat_gateway.gw.*.id, count.index)
+  }
+}
+
+# Explicitly associate the newly created route tables to the private subnets (so they don't default to the main route table)
+resource "aws_route_table_association" "private" {
+  count          = 2
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = element(aws_route_table.private.*.id, count.index)
+}
+
 module "api" {
   source             = "./api"
   env                = var.env
