32e5507fa7659cc66d634b88be52525bca0fca29
Lucas Kjaero <lucas@lucaskjaero.com>
2020-02-05 23:24:04 -0800

    Manually specify the CIDR blocks

--- infrastructure/api/main.tf
+++ infrastructure/api/main.tf
@@ -86,7 +86,7 @@ resource "aws_security_group" "database" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = [data.aws_subnet.private.*.cidr_block]
+    cidr_blocks = var.private_subnet_cidrs
   }
 
   tags = {
