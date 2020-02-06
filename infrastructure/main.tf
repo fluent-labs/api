b97bb9f7251b72bb3069c3967e32c68509afe386
Lucas Kjaero <lucas@lucaskjaero.com>
2020-02-05 22:18:29 -0800

    Route tables for private network

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -110,6 +110,25 @@ resource "aws_nat_gateway" "gw" {
   allocation_id = element(aws_eip.gw.*.id, count.index)
 }
 
+# Create a new route table for the private subnets
+# And make it route non-local traffic through the NAT gateway to the internet
+resource "aws_route_table" "private" {
+  count  = 2
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = element(aws_nat_gateway.gw.*.id, count.index)
+  }
+}
+
+# Explicitly associate the newly created route tables to the private subnets (so they don't default to the main route table)
+resource "aws_route_table_association" "private" {
+  count          = 2
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = element(aws_route_table.private.*.id, count.index)
+}
+
 module "api" {
   source             = "./api"
   env                = var.env
