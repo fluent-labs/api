4e81c06e090a8d22cda363607126fb6775d6cfc7
Lucas Kjaero <lucas@lucaskjaero.com>
2020-02-05 22:15:41 -0800

    Route tables for public subnet

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -87,6 +87,13 @@ resource "aws_network_acl_rule" "inbound_ssh_traffic" {
   to_port        = 22
 }
 
+# Route the public subnet traffic through the IGW
+resource "aws_route" "internet_access" {
+  route_table_id         = "${aws_vpc.main.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.gw.id}"
+}
+
 module "api" {
   source             = "./api"
   env                = var.env
