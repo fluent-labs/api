9193cf3089de41ab37ad06a58f3111120041841e
Lucas Kjaero <lucas@lucaskjaero.com>
2020-02-07 23:52:11 -0800

    Explicitly route through internet gateway

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -118,6 +118,25 @@ resource "aws_nat_gateway" "gw" {
   }
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "API-Public-Subnet-${count.index}"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = 2
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+
 # Create a new route table for the private subnets
 # And make it route non-local traffic through the NAT gateway to the internet
 resource "aws_route_table" "private" {
