71e6c54ac87e93f62d49c5d0976593752033a033
Lucas Kjaero <lucas@lucaskjaero.com>
2020-02-09 14:24:12 -0800

    Rename the module so it isn't confused with environments

--- infrastructure.tf
+++ infrastructure.tf
@@ -30,7 +30,7 @@ variable "github_token" {
   default = "KN4rxjPOfxRk3uo7fD928e6nt12jzrvy5t90fp7Snlp63ckc0rRZTglirGt+WiB6"
 }
 
-module "prod" {
+module "foreign_language_reader" {
   source          = "./infrastructure"
   env             = "prod"
   instance_size   = "t2.micro"
