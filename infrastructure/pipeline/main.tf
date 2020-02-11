adb05d09c5a20f021e6304e0c8c5efcb9c065be7
Lucas Kjaero <lucas@lucaskjaero.com>
2020-02-10 22:54:42 -0800

    Perhaps upgrade the build image
    Docker is missing

--- infrastructure/pipeline/main.tf
+++ infrastructure/pipeline/main.tf
@@ -47,7 +47,7 @@ resource "aws_codebuild_project" "api_build" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:1.0"
+    image                       = "aws/codebuild/standard:2.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
 
