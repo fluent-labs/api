47ae9d69aad849a9fa01f9f6940169781f97146f
Lucas Kjaero <lucas@lucaskjaero.com>
2020-02-13 23:42:53 -0800

    Remove duplicates

--- language-service/language_service/nlp/chinese/__init__.py
+++ language-service/language_service/nlp/chinese/__init__.py
@@ -1,5 +1,5 @@
 import thulac
-from common import is_not_punctuation
+from ..common import is_not_punctuation
 
 parser = thulac.thulac()
 
@@ -36,11 +36,11 @@ part_of_speech_mapping = {
 
 
 def tag_text(text):
-    for token in parser.cut(text).split():
-        word, tag = token.split("_")
-        if is_not_punctuation(word):
-            yield {"token": word, "tag": part_of_speech_mapping[tag], "lemma": word}
+    # Allows not sending duplicates which is pretty important
+    words = {word: part_of_speech_mapping[tag] for word, tag in parser.cut(text) if is_not_punctuation(word)}
+    for word in words.keys():
+        yield {"token": word, "tag": words[word], "lemma": word}
 
 
 def tag_chinese(text):
-    return list(set(tag_text(text)))
+    return list(tag_text(text))
