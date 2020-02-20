3307e03a0409947e940e0552f669d7980d0b6059
Lucas Kjaero-Zhang <lucaskjaero@users.noreply.github.com>
2020-02-19 22:55:28 -0800

    Productionize language service container (#81)
    * Cython is a hidden dependency spacy
    
    * Don't use poetry to install cython
    
    It is needed before running poetry
    
    * Checkpoint
    
    * Need dependencies to build poetry
    
    * Don't include the builds
    
    * Don't time out when building build container
    
    * Don't include hashes
    
    They break the build since poetry doesn't track them for url dependencies.
    
    * Maybe tag latest?
    
    * Remove commented out code
    
    * Switch to jieba instead of thulac
    
    * Replace jieba in service
    
    * Don't use alpine for base image

--- language-service/language_service/nlp/chinese/__init__.py
+++ language-service/language_service/nlp/chinese/__init__.py
@@ -1,38 +1,44 @@
-import thulac
+from collections import defaultdict
+import jieba
+import jieba.posseg as parser
 from ..common import is_not_punctuation, Word
 
-parser = thulac.thulac()
+jieba.enable_paddle()
 
-part_of_speech_mapping = {
-    "n": "名词",
-    "np": "人名",
-    "ns": "地名",
-    "ni": "机构名",
-    "nz": "其它专名",
-    "m": "数词",
-    "q": "量词",
-    "mq": "数量词",
-    "t": "时间词",
-    "f": "方位词",
-    "s": "处所词",
-    "v": "动词",
-    "a": "形容词",
-    "d": "副词",
-    "h": "前接成分",
-    "k": "后接成分",
-    "i": "习语",
-    "j": "简称",
-    "r": "代词",
-    "c": "连词",
-    "p": "介词",
-    "u": "助词",
-    "y": "语气助词",
-    "e": "叹词",
-    "o": "拟声词",
-    "g": "语素",
-    "w": "标点",
-    "x": "其它",
-}
+part_of_speech_mapping = defaultdict(lambda: "不知道词性")
+
+part_of_speech_mapping.update(
+    {
+        "n": "普通名词",
+        "f": "方位名词",
+        "s": "处所名词",
+        "t": "时间",
+        "nr": "人名",
+        "ns": "地名",
+        "nt": "机构名",
+        "nw": "作品名",
+        "nz": "其他专名",
+        "v": "普通动词",
+        "vd": "动副词",
+        "vn": "名动词",
+        "a": "形容词",
+        "ad": "副形词",
+        "an": "名形词",
+        "d": "副词",
+        "m": "数量词",
+        "q": "量词",
+        "r": "代词",
+        "p": "介词",
+        "c": "连词",
+        "u": "助词",
+        "xc": "其他虚词",
+        "w": "标点符号",
+        "PER": "人名",
+        "LOC": "地名",
+        "ORG": "机构名",
+        "TIME": "时间",
+    }
+)
 
 
 def tag_chinese(text):
