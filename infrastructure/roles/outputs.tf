1e1d01c83cb4d6aa87c61542d0fae3e96ebf7604
Lucas Kjaero-Zhang <lucaskjaero@users.noreply.github.com>
2020-02-21 08:29:40 -0800

    Autoscale services (#83)
    * Create role for autoscaling
    
    * Minor change
    
    * Autoscaling role
    
    * Syntax updates
    
    * Autoscale language service

--- infrastructure/roles/outputs.tf
+++ infrastructure/roles/outputs.tf
@@ -2,6 +2,10 @@ output "fargate_role" {
   value = aws_iam_role.fargate_task_exec.arn
 }
 
+output "fargate_autoscale_role" {
+  value = aws_iam_role.fargate_autoscale_role.arn
+}
+
 output "codebuild_role" {
   value = aws_iam_role.codebuild_role.arn
 }
