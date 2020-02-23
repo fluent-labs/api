a0e75e51925153f7b2777b7525bf99ad25e2fd23
Lucas Kjaero <lucas@lucaskjaero.com>
2020-02-22 21:02:23 -0800

    Container registries in AWS

---
+++ infrastructure/terraform/aws/main.tf
@@ -0,0 +1,31 @@
+resource "aws_iam_access_key" "github" {
+  user    = aws_iam_user.github.name
+}
+
+resource "aws_iam_user" "github" {
+  name = "foreign-language-reader-github"
+}
+
+resource "aws_iam_access_key" "kubernetes" {
+  user    = aws_iam_user.kubernetes.name
+}
+
+resource "aws_iam_user" "kubernetes" {
+  name = "foreign-language-reader-kubernetes"
+}
+
+module "api_registry" {
+  source      = "./container_registry"
+  name        = "foreign-language-reader-api"
+  image_count = 5
+  push_users  = [aws_iam_user.github.name]
+  pull_users  = [aws_iam_user.kubernetes.name]
+}
+
+module "language_service_registry" {
+  source      = "./container_registry"
+  name        = "	foreign-language-reader-language-service"
+  image_count = 5
+  push_users  = [aws_iam_user.github.name]
+  pull_users  = [aws_iam_user.kubernetes.name]
+}
