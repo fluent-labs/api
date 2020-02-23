183c6e036e1e61f29686c4dab9eecbe6cd23ab24
Lucas Kjaero <lucas@lucaskjaero.com>
2020-02-22 21:31:26 -0800

    Autoscaling

--- infrastructure/terraform/digitalocean/main.tf
+++ infrastructure/terraform/digitalocean/main.tf
@@ -7,7 +7,9 @@ resource "digitalocean_kubernetes_cluster" "foreign_language_reader" {
   node_pool {
     name       = "worker-pool"
     size       = "s-1vcpu-2gb"
-    node_count = 3
+    auto_scale = true
+    min_nodes  = 1
+    max_nodes  = 2
   }
 }
 
