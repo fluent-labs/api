77451005017b43cc1bf0ae5fc2129b943a0098d0
Lucas Kjaero <lucas@lucaskjaero.com>
2020-02-22 23:54:04 -0800

    Give global permissions to access ECR

--- infrastructure/terraform/aws/main.tf
+++ infrastructure/terraform/aws/main.tf
@@ -14,6 +14,27 @@ resource "aws_iam_user" "kubernetes" {
   name = "foreign-language-reader-kubernetes"
 }
 
+data "aws_iam_policy_document" "ecr_user" {
+  statement {
+    actions   = ["ecr:GetAuthorizationToken"]
+    effect    = "Allow"
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "ecr_user" {
+  name        = "ecr-user"
+  description = "IAM policy for getting access to ECR"
+
+  policy = data.aws_iam_policy_document.ecr_user.json
+}
+
+resource "aws_iam_policy_attachment" "ecr_user_attach" {
+  name       = "ecr_user"
+  users      = [aws_iam_user.github.name, aws_iam_user.kubernetes.name]
+  policy_arn = aws_iam_policy.push_access.arn
+}
+
 module "api_registry" {
   source      = "./container_registry"
   name        = "foreign-language-reader-api"
