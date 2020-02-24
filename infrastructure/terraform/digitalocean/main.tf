7ad6d38dfb312aca9f5048f66a8abc572d14c747
Lucas Kjaero <lucas@lucaskjaero.com>
2020-02-23 21:58:20 -0800

    Set up DO database

--- infrastructure/terraform/digitalocean/main.tf
+++ infrastructure/terraform/digitalocean/main.tf
@@ -1,3 +1,5 @@
+# Configure K8s
+
 resource "digitalocean_project" "foreign_language_reader" {
   name        = "foreign-language-reader"
   description = "Read text in different languages."
@@ -27,4 +29,34 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(
     digitalocean_kubernetes_cluster.foreign_language_reader.kube_config[0].cluster_ca_certificate
   )
+}
+
+# Configure database
+
+resource "digitalocean_database_cluster" "api_mysql" {
+  name       = "foreign-language-reader"
+  engine     = "mysql"
+  version    = "8"
+  size       = "db-s-1vcpu-1gb"
+  region     = "sfo2"
+  node_count = 1
+}
+
+resource "digitalocean_database_firewall" "allow_kubernetes" {
+  cluster_id = digitalocean_database_cluster.api_mysql.id
+
+  rule {
+    type  = "k8s"
+    value = digitalocean_kubernetes_cluster.foreign_language_reader.id
+  }
+}
+
+resource "digitalocean_database_user" "api_user" {
+  cluster_id = digitalocean_database_cluster.api_mysql.id
+  name       = "api"
+}
+
+resource "digitalocean_database_db" "api_database" {
+  cluster_id = digitalocean_database_cluster.foreign_language_reader.id
+  name       = "foreign-language-reader"
 }
