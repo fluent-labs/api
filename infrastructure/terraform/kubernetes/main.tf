6bdaeea372221e6ba7ee35f87ff1da961fcdaab1
Lucas Kjaero <lucas@lucaskjaero.com>
2020-02-28 07:55:59 -0800

    Unpin the version to validate connectivity

--- infrastructure/terraform/kubernetes/main.tf
+++ infrastructure/terraform/kubernetes/main.tf
@@ -12,6 +12,4 @@ resource "helm_release" "nginx_ingress" {
   name              = "nginx-ingress"
   repository        = "https://helm.nginx.com/stable"
   chart             = "nginx-stable/nginx-ingress"
-  version           = "0.4.2"
-  dependency_update = true
 }
