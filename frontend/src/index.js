a7407ec28e3a1f65deaad0513fc441cc7d9c6fd7
Lucas Kjaero <lucas@lucaskjaero.com>
2020-03-01 22:39:40 -0800

    Detect environment and route it to the right place

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,9 +10,15 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
 
+// Environment detection
+let apiUrl = "https://api.foreignlanguagereader.com/api/";
+if (window.location.hostname === "localhost") {
+  apiUrl = "http://localhost:4000/api/";
+}
+
 const cache = new InMemoryCache();
 const link = new HttpLink({
-  uri: "http://localhost:4000/"
+  uri: apiUrl
 });
 
 const client = new ApolloClient({
