4d7c748f85e39fa85688aadbdde422b3faf6b0a1
Lucas Kjaero <lucas@lucaskjaero.com>
2020-03-02 07:56:30 -0800

    Resolve word type based on language tag

--- api/lib/api_web/schema/domain_types.ex
+++ api/lib/api_web/schema/domain_types.ex
@@ -15,7 +15,9 @@ defmodule ApiWeb.Schema.DomainTypes do
     field :definitions, %Absinthe.Type.List{of_type: non_null(:string)}
 
     resolve_type fn
-      %{pinyin: _}, _ -> :chinese_word
+      %{language: :chinese}, _ -> :chinese_word
+      %{language: :english}, _ -> :english_word
+      %{language: :spanish}, _ -> :spanish_word
       _, _ -> :generic_word
     end
   end
@@ -40,6 +42,28 @@ defmodule ApiWeb.Schema.DomainTypes do
     field :definitions, %Absinthe.Type.List{of_type: non_null(:string)}
     field :hsk, :integer
     field :pinyin, %Absinthe.Type.List{of_type: non_null(:string)}
+
+    interface :word
+  end
+
+  object :english_word do
+    field :id, non_null(:id)
+    field :language, non_null(:language)
+    field :text, non_null(:string)
+    field :part_of_speech, :string
+    field :lemma, :string
+    field :definitions, %Absinthe.Type.List{of_type: non_null(:string)}
+
+    interface :word
+  end
+
+  object :spanish_word do
+    field :id, non_null(:id)
+    field :language, non_null(:language)
+    field :text, non_null(:string)
+    field :part_of_speech, :string
+    field :lemma, :string
+    field :definitions, %Absinthe.Type.List{of_type: non_null(:string)}
 
     interface :word
   end
