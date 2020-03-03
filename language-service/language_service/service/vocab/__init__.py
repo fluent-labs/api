592a4192246b0292b07ff3cff0547ca81a111abd
Lucas Kjaero <lucas@lucaskjaero.com>
2020-03-02 22:37:06 -0800

    Parse definition

--- language-service/language_service/service/vocab/__init__.py
+++ language-service/language_service/service/vocab/__init__.py
@@ -1,7 +1,23 @@
 from wiktionaryparser import WiktionaryParser
+from dto import Definition
 
 parser = WiktionaryParser()
 
 
 def get_definition(language, word):
-    return parser.fetch(word, language)
+    response = parser.fetch(word, language)
+
+    definitions = []
+    for entry in response:
+        if "definitions" in entry:
+            for definition in entry["definitions"]:
+                definitions.append(parse_definition(definition))
+
+    return definitions
+
+
+def parse_definition(definition):
+    subdefinitions = definition["text"] if "text" in definition else None
+    tag = definition["partOfSpeech"] if "partOfSpeech" in definition else None
+    examples = definition["examples"] if "examples" in definition else None
+    return Definition(subdefinitions=subdefinitions, tag=tag, examples=examples)
