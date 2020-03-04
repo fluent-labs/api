9a2211662ae3e9208a0fc1807cbd3de20bfff86a
Lucas Kjaero <lucas@lucaskjaero.com>
2020-03-04 08:55:32 -0800

    Don't use IO.Inspect in logger

--- api/lib/api/clients/common.ex
+++ api/lib/api/clients/common.ex
@@ -4,10 +4,14 @@ defmodule Tesla.Middleware.RequestLogger do
   Logs the requests made by Tesla clients
   """
 
-  def call(env, next, _) do
-    env
-    |> IO.inspect()
-    |> Tesla.run(next)
+  def call(%Tesla.Env{url: url, body: body, method: method} = env, next, _) do
+    case method do
+      :get -> IO.puts("Calling " <> url)
+      :post ->
+        {:ok, body_json} = Jason.encode(body)
+        IO.puts("Calling " <> url <> " with body: " <> body_json)
+    end
+    Tesla.run(env, next)
   end
 end
 
@@ -18,8 +22,12 @@ defmodule Tesla.Middleware.ResponseLogger do
   """
 
   def call(env, next, _) do
-    env
-    |> Tesla.run(next)
-    |> IO.inspect()
+    case Tesla.run(env, next) do
+      {:ok, successful_env} ->
+        %Tesla.Env{url: url} = successful_env
+        IO.puts("Successful response from " <> url)
+        {:ok, successful_env}
+      {:error, err} -> IO.puts(err)
+    end
   end
 end
