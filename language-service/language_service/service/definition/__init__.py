6b61210fa70c74b645396d7e7b6fe258bb740c31
Lucas Kjaero-Zhang <lucaskjaero@users.noreply.github.com>
2020-03-04 22:24:11 -0800

    Error handling when multiple definitions are requested (#114)
    We don't want one bad lookup to fail the entire transaction.

--- language-service/language_service/service/definition/__init__.py
+++ language-service/language_service/service/definition/__init__.py
@@ -13,32 +13,7 @@ from language_service.service.definition.wiktionary import (
 
 def get_definitions(language, word):
     if language == "CHINESE":
-        wiktionary_definitions = get_wiktionary_definitions(language, word)
-
-        cedict_definition = get_cedict_definitions(word)
-        if cedict_definition:
-            print("Attaching cedict definition for %s" % word)
-            definitions = []
-            for definition in wiktionary_definitions:
-                # The CEDICT definitions are more focused than wiktionary so we should prefer them.
-                subdefinitions = (
-                    cedict_definition["definitions"]
-                    if "definitions" in cedict_definition
-                    else definition.subdefinitions
-                )
-
-                improved_definition = ChineseDefinition(
-                    subdefinitions=subdefinitions,
-                    tag=definition.tag,
-                    examples=definition.examples,
-                    pinyin=cedict_definition["pinyin"],
-                    simplified=cedict_definition["simplified"],
-                    traditional=cedict_definition["traditional"],
-                )
-                definitions.append(improved_definition)
-            return definitions
-        else:
-            return wiktionary_definitions
+        return get_chinese_definitions(word)
 
     elif language == "ENGLISH":
         return get_wiktionary_definitions(language, word)
@@ -48,3 +23,42 @@ def get_definitions(language, word):
 
     else:
         raise NotImplementedError("Unknown language requested: %s")
+
+
+def get_chinese_definitions(word):
+    wiktionary_definitions = get_wiktionary_definitions("CHINESE", word)
+    cedict_definition = get_cedict_definitions(word)
+
+    if wiktionary_definitions is not None and cedict_definition is not None:
+        print("CHINESE - Found wiktionary and cedict definitions for %s" % word)
+        definitions = []
+        for definition in wiktionary_definitions:
+            # The CEDICT definitions are more focused than wiktionary so we should prefer them.
+            subdefinitions = (
+                cedict_definition.subdefinitions
+                if cedict_definition.subdefinitions is not None
+                else definition.subdefinitions
+            )
+
+            improved_definition = ChineseDefinition(
+                subdefinitions=subdefinitions,
+                tag=definition.tag,
+                examples=definition.examples,
+                pinyin=cedict_definition.pinyin,
+                simplified=cedict_definition.simplified,
+                traditional=cedict_definition.traditional,
+            )
+            definitions.append(improved_definition)
+        return definitions
+
+    elif wiktionary_definitions is not None and cedict_definition is None:
+        print("CHINESE - Only found wiktionary definition for %s" % word)
+        return wiktionary_definitions
+
+    elif wiktionary_definitions is None and cedict_definition is not None:
+        print("CHINESE - Only found cedict definition for %s" % word)
+        return [cedict_definition]
+
+    else:
+        print("CHINESE - No definition found for %s" % word)
+        return None
--- language-service/language_service/service/definition/cedict/__init__.py
+++ language-service/language_service/service/definition/cedict/__init__.py
@@ -3,6 +3,7 @@ CEDICT is an open source Chinese dictionary.
 This class provides definitions
 """
 import json
+from language_service.dto import ChineseDefinition
 
 definitions = {}
 with open("content/cedict.json") as cedict:
@@ -13,6 +14,12 @@ with open("content/cedict.json") as cedict:
 
 def get_definitions(word):
     if word in definitions:
-        return definitions[word]
+        definition = definitions[word]
+        return ChineseDefinition(
+            subdefinitions=definition["definitions"],
+            pinyin=definition["pinyin"],
+            simplified=definition["simplified"],
+            traditional=definition["traditional"],
+        )
     else:
         return None
