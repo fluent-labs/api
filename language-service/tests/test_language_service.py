4e50c276177ca4856b3ee22658149deb8f2750ea
Lucas Kjaero <lucas@lucaskjaero.com>
2020-03-07 08:22:13 -0800

    Get multiple definitions end to end test

--- language-service/tests/test_language_service.py
+++ language-service/tests/test_language_service.py
@@ -1,4 +1,5 @@
 from language_service.LanguageService import app
+from language_service.dto.definition import Definition
 
 
 def test_health(mocker):
@@ -24,3 +25,43 @@ def test_tagging(mocker):
         {"lemma": "test", "tag": "NOUN", "token": "test"},
     ]
     assert response.status == "200 OK"
+
+
+this_definition = Definition(subdefinitions=["This"])
+is_definition = Definition(subdefinitions=["is"])
+a_definition = Definition(subdefinitions=["a"])
+test_definition = Definition(subdefinitions=["test"])
+
+
+def get_definitions_successfully_mock(language, word):
+    if word == "This":
+        return [this_definition]
+    elif word == "is":
+        return [is_definition]
+    elif word == "a":
+        return [a_definition]
+    elif word == "test":
+        return [test_definition]
+
+
+def test_definitions(mocker):
+    get_english_definitions = mocker.patch(
+        "language_service.service.definition.language.english.Wiktionary"
+    )
+    wiktionary = get_english_definitions.return_value
+    wiktionary.get_definitions.side_effect = get_definitions_successfully_mock
+
+    client = app.test_client()
+    response = client.post(
+        "/api/v1/definitions/ENGLISH/",
+        json={"words": ["This", "is", "a", "test"]},
+        headers={"Authorization": "local"},
+    )
+
+    assert response.status == "200 OK"
+    assert response.get_json() == {
+        "This": [{"examples": None, "subdefinitions": ["This"], "tag": ""}],
+        "a": [{"examples": None, "subdefinitions": ["a"], "tag": ""}],
+        "is": [{"examples": None, "subdefinitions": ["is"], "tag": ""}],
+        "test": [{"examples": None, "subdefinitions": ["test"], "tag": ""}],
+    }
