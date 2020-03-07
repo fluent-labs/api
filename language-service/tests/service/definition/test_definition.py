d850b921f5ea069fd2fd54d0da2791df230d42a9
Lucas Kjaero <lucas@lucaskjaero.com>
2020-03-06 23:18:42 -0800

    Test the multi definition method

--- language-service/tests/service/definition/test_definition.py
+++ language-service/tests/service/definition/test_definition.py
@@ -1,5 +1,71 @@
 import pytest
-from language_service.service.definition import get_definitions
+from testfixtures import compare
+
+from language_service.dto.definition import Definition
+from language_service.service.definition import (
+    get_definitions,
+    get_definitions_for_group,
+)
+
+this_definition = Definition(subdefinitions=["This"])
+is_definition = Definition(subdefinitions=["is"])
+a_definition = Definition(subdefinitions=["a"])
+test_definition = Definition(subdefinitions=["test"])
+
+
+def get_definitions_successfully_mock(word):
+    if word == "This":
+        return this_definition
+    elif word == "is":
+        return is_definition
+    elif word == "a":
+        return a_definition
+    elif word == "test":
+        return test_definition
+
+
+def test_get_definitions_for_group_happy_path(mocker):
+    get_english_definitions = mocker.patch(
+        "language_service.service.definition.get_english_definitions"
+    )
+    get_english_definitions.side_effect = get_definitions_successfully_mock
+
+    result = get_definitions_for_group("ENGLISH", ["This", "is", "a", "test"])
+
+    compare(
+        result,
+        [
+            ("This", this_definition),
+            ("is", is_definition),
+            ("a", a_definition),
+            ("test", test_definition),
+        ],
+    )
+
+
+def get_definitions_with_failures_mock(word):
+    if word == "This":
+        return this_definition
+    elif word == "is":
+        return None
+    elif word == "a":
+        return a_definition
+    elif word == "test":
+        return None
+
+
+def test_get_definitions_for_group_with_errors(mocker):
+    get_english_definitions = mocker.patch(
+        "language_service.service.definition.get_english_definitions"
+    )
+    get_english_definitions.side_effect = get_definitions_with_failures_mock
+
+    result = get_definitions_for_group("ENGLISH", ["This", "is", "a", "test"])
+
+    compare(
+        result,
+        [("This", this_definition), ("is", None), ("a", a_definition), ("test", None),],
+    )
 
 
 def test_can_pass_through_chinese_definitions(mocker):
