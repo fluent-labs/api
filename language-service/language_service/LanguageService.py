57a82090a6d9971bac56423729c115ccda5a09ce
Lucas Kjaero <lucas@lucaskjaero.com>
2020-03-06 18:02:09 -0800

    Simplify the controllers

--- language-service/language_service/LanguageService.py
+++ language-service/language_service/LanguageService.py
@@ -3,23 +3,19 @@ import os
 from flask import Flask
 from flask_restful import Api
 
-from language_service.controller import (
-    DefinitionController,
-    DefinitionMultipleController,
-    DocumentController,
-    HealthController,
-)
+from language_service.controller.definition import DefinitionController
+from language_service.controller.document import DocumentController
+from language_service.controller.health import HealthController
 
 app = Flask(__name__)
 api = Api(app)
 
-API_BASE = os.getenv("APPLICATION_ROOT", "/")
+API_BASE = os.getenv("APPLICATION_ROOT", "/api/")
 
 api.add_resource(
-    DefinitionController, API_BASE + "/v1/definition/<string:language>/<string:word>"
-)
-api.add_resource(
-    DefinitionMultipleController, API_BASE + "/v1/definitions/<string:language>/"
+    DefinitionController,
+    API_BASE + "/v1/definitions/<string:language>/",
+    API_BASE + "/v1/definition/<string:language>/<string:word>",
 )
 api.add_resource(
     DocumentController, API_BASE + "/v1/tagging/<string:language>/document"
