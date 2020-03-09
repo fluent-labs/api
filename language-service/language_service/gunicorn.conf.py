001f4ca76341fafc727c3ecb94b9b59ec7a1d621
Lucas Kjaero <lucas@lucaskjaero.com>
2020-03-08 22:06:41 -0700

    Make the security checker happy

--- language-service/language_service/gunicorn.conf.py
+++ language-service/language_service/gunicorn.conf.py
@@ -1,10 +1,11 @@
 # Mostly necessary to keep the health check from failing when a long running call happens
+# If I revisit this then this stack overflow was pretty helpful:
+# https://pythonspeed.com/articles/gunicorn-in-docker/
 workers = 2
 threads = 4
 worker_class = "gevent"
-worker_tmp_dir = "/dev/shm"
-# If I revisit this then this stack overflow was pretty helpful:
-# https://pythonspeed.com/articles/gunicorn-in-docker/
+worker_tmp_dir = "/dev/shm"  # nosec This is low risk
+# This application is pretty well locked down in a container
 
 # We should filter the logs at the cluster level
 loglevel = "debug"
