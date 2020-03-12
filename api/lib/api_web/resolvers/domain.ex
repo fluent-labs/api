0a091460798c620ba94acc0fed5452f52e2b00db
Lucas Kjaero <lucas@lucaskjaero.com>
2020-03-11 22:53:50 -0700

    Request definitions in groups of 10

--- api/lib/api_web/resolvers/domain.ex
+++ api/lib/api_web/resolvers/domain.ex
@@ -4,11 +4,25 @@ defmodule ApiWeb.Resolvers.Domain do
   """
   alias Api.Clients
 
+  defp get_definitions(language, tokens) do
+    case Clients.LanguageService.definitions(language, tokens) do
+      {:ok, definitions} -> definitions
+      _ -> :error
+    end
+  end
+
   def get_words_in_text(_parent, %{language: language, text: text}, _resolution) do
     {:ok, words} = Clients.LanguageService.tag(language, text)
-    tokens = Enum.map(words, fn word -> Map.fetch!(word, :token) end)
-    {:ok, definitions} = Clients.LanguageService.definitions(language, tokens)
 
+    # Request definitions in batches of 10
+    # Too many words in a request will still cause problems
+    definitions =
+      Enum.map(words, fn word -> Map.fetch!(word, :token) end)
+      |> Enum.chunk_every(10)
+      |> Enum.flat_map(fn tokens -> get_definitions(language, tokens) end)
+      |> Map.new
+
+    # Join everything together.
     processed_words =
       words
       |> Enum.map(fn word -> Map.put(word, :language, language) end)
