252bc58253f51ff654ce482ce15d6c339af47bcb
Lucas Kjaero-Zhang <lucaskjaero@users.noreply.github.com>
2020-03-13 22:44:21 -0700

    Reconfigure K8s cluster (#133)
    * RECREATE ALL THE THINGS
    
    * Ok the load balancer isn't ready yet
    
    * Don't edit the project

--- infrastructure/terraform/kubernetes_cluster_infrastructure/main.tf
+++ infrastructure/terraform/kubernetes_cluster_infrastructure/main.tf
@@ -2,6 +2,7 @@ data "digitalocean_domain" "main" {
   name = var.domain_name
 }
 
+# TODO actually plug in the URN when terraform supports it.
 resource "digitalocean_project" "foreign_language_reader" {
   name        = "foreign-language-reader"
   description = "Read text in different languages."
@@ -10,13 +11,14 @@ resource "digitalocean_project" "foreign_language_reader" {
   resources = [
     digitalocean_database_cluster.api_mysql.urn,
     data.digitalocean_domain.main.urn,
-    digitalocean_database_cluster.language_service_cache.urn
+    digitalocean_database_cluster.language_service_cache.urn,
+    "do:kubernetes:a5be20d1-0248-460b-a1e1-355d6a5a8c64"
   ]
 }
 
-# data "digitalocean_kubernetes_cluster" "foreign_language_reader" {
-#   name = var.cluster_name
-# }
+data "digitalocean_kubernetes_cluster" "foreign_language_reader" {
+  name = var.cluster_name
+}
 
 # Configure database
 
@@ -29,14 +31,14 @@ resource "digitalocean_database_cluster" "api_mysql" {
   node_count = 1
 }
 
-# resource "digitalocean_database_firewall" "allow_kubernetes" {
-#   cluster_id = digitalocean_database_cluster.api_mysql.id
-#
-#   rule {
-#     type  = "k8s"
-#     value = data.digitalocean_kubernetes_cluster.foreign_language_reader.id
-#   }
-# }
+resource "digitalocean_database_firewall" "allow_kubernetes" {
+  cluster_id = digitalocean_database_cluster.api_mysql.id
+
+  rule {
+    type  = "k8s"
+    value = data.digitalocean_kubernetes_cluster.foreign_language_reader.id
+  }
+}
 
 resource "digitalocean_database_user" "api_user" {
   cluster_id = digitalocean_database_cluster.api_mysql.id
@@ -48,20 +50,20 @@ resource "digitalocean_database_db" "api_database" {
   name       = "foreign-language-reader"
 }
 
-# resource "kubernetes_secret" "api_database_credentials" {
-#   metadata {
-#     name = "api-database-credentials"
-#   }
-#
-#   data = {
-#     username          = digitalocean_database_user.api_user.name
-#     password          = digitalocean_database_user.api_user.password
-#     host              = digitalocean_database_cluster.api_mysql.private_host
-#     port              = digitalocean_database_cluster.api_mysql.port
-#     database          = digitalocean_database_db.api_database.name
-#     connection_string = "ecto://${digitalocean_database_user.api_user.name}:${digitalocean_database_user.api_user.password}@${digitalocean_database_cluster.api_mysql.private_host}:${digitalocean_database_cluster.api_mysql.port}/${digitalocean_database_db.api_database.name}"
-#   }
-# }
+resource "kubernetes_secret" "api_database_credentials" {
+  metadata {
+    name = "api-database-credentials"
+  }
+
+  data = {
+    username          = digitalocean_database_user.api_user.name
+    password          = digitalocean_database_user.api_user.password
+    host              = digitalocean_database_cluster.api_mysql.private_host
+    port              = digitalocean_database_cluster.api_mysql.port
+    database          = digitalocean_database_db.api_database.name
+    connection_string = "ecto://${digitalocean_database_user.api_user.name}:${digitalocean_database_user.api_user.password}@${digitalocean_database_cluster.api_mysql.private_host}:${digitalocean_database_cluster.api_mysql.port}/${digitalocean_database_db.api_database.name}"
+  }
+}
 
 # Cache for language service
 
@@ -75,28 +77,28 @@ resource "digitalocean_database_cluster" "language_service_cache" {
   node_count      = 1
 }
 
-# resource "digitalocean_database_firewall" "allow_kubernetes_to_redis" {
-#   cluster_id = digitalocean_database_cluster.language_service_cache.id
-#
-#   rule {
-#     type  = "k8s"
-#     value = data.digitalocean_kubernetes_cluster.foreign_language_reader.id
-#   }
-# }
+resource "digitalocean_database_firewall" "allow_kubernetes_to_redis" {
+  cluster_id = digitalocean_database_cluster.language_service_cache.id
 
-# resource "kubernetes_secret" "language_service_cache_credentials" {
-#   metadata {
-#     name = "language-service-cache-credentials"
-#   }
-#
-#   data = {
-#     username          = digitalocean_database_cluster.language_service_cache.user
-#     password          = digitalocean_database_cluster.language_service_cache.password
-#     host              = digitalocean_database_cluster.language_service_cache.private_host
-#     port              = digitalocean_database_cluster.language_service_cache.port
-#     connection_string = "rediss://${digitalocean_database_cluster.language_service_cache.user}:${digitalocean_database_cluster.language_service_cache.password}@${digitalocean_database_cluster.language_service_cache.private_host}:${digitalocean_database_cluster.language_service_cache.port}/0"
-#   }
-# }
+  rule {
+    type  = "k8s"
+    value = data.digitalocean_kubernetes_cluster.foreign_language_reader.id
+  }
+}
+
+resource "kubernetes_secret" "language_service_cache_credentials" {
+  metadata {
+    name = "language-service-cache-credentials"
+  }
+
+  data = {
+    username          = digitalocean_database_cluster.language_service_cache.user
+    password          = digitalocean_database_cluster.language_service_cache.password
+    host              = digitalocean_database_cluster.language_service_cache.private_host
+    port              = digitalocean_database_cluster.language_service_cache.port
+    connection_string = "rediss://${digitalocean_database_cluster.language_service_cache.user}:${digitalocean_database_cluster.language_service_cache.password}@${digitalocean_database_cluster.language_service_cache.private_host}:${digitalocean_database_cluster.language_service_cache.port}/0"
+  }
+}
 
 # Configure networking
 
--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,29 +1,29 @@
 # Hold K8s configuration in an intermediate level
 # Terraform currently cannot create a cluster and use it to set up a provider on the same leve.
 
-# data "digitalocean_kubernetes_cluster" "foreign_language_reader" {
-#   name = var.cluster_name
-# }
-#
-# provider "kubernetes" {
-#   load_config_file = false
-#   host             = data.digitalocean_kubernetes_cluster.foreign_language_reader.endpoint
-#   token            = data.digitalocean_kubernetes_cluster.foreign_language_reader.kube_config[0].token
-#   cluster_ca_certificate = base64decode(
-#     data.digitalocean_kubernetes_cluster.foreign_language_reader.kube_config[0].cluster_ca_certificate
-#   )
-# }
-#
-# provider "helm" {
-#   kubernetes {
-#     load_config_file = false
-#     host             = data.digitalocean_kubernetes_cluster.foreign_language_reader.endpoint
-#     token            = data.digitalocean_kubernetes_cluster.foreign_language_reader.kube_config[0].token
-#     cluster_ca_certificate = base64decode(
-#       data.digitalocean_kubernetes_cluster.foreign_language_reader.kube_config[0].cluster_ca_certificate
-#     )
-#   }
-# }
+data "digitalocean_kubernetes_cluster" "foreign_language_reader" {
+  name = var.cluster_name
+}
+
+provider "kubernetes" {
+  load_config_file = false
+  host             = data.digitalocean_kubernetes_cluster.foreign_language_reader.endpoint
+  token            = data.digitalocean_kubernetes_cluster.foreign_language_reader.kube_config[0].token
+  cluster_ca_certificate = base64decode(
+    data.digitalocean_kubernetes_cluster.foreign_language_reader.kube_config[0].cluster_ca_certificate
+  )
+}
+
+provider "helm" {
+  kubernetes {
+    load_config_file = false
+    host             = data.digitalocean_kubernetes_cluster.foreign_language_reader.endpoint
+    token            = data.digitalocean_kubernetes_cluster.foreign_language_reader.kube_config[0].token
+    cluster_ca_certificate = base64decode(
+      data.digitalocean_kubernetes_cluster.foreign_language_reader.kube_config[0].cluster_ca_certificate
+    )
+  }
+}
 
 # Deploy user for github actions
 # Will be given ECR push and S3 sync access
@@ -53,9 +53,9 @@ module "kubernetes_cluster_infrastructure" {
 }
 
 # Installed apps, some secrets, service users, etc.
-# module "kubernetes_config" {
-#   source = "./kubernetes_config"
-# }
+module "kubernetes_config" {
+  source = "./kubernetes_config"
+}
 
 module "frontend" {
   source       = "./static_bucket"
