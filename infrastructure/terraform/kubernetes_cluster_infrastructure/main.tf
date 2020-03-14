10bb0d4f6056dfcfa77f4079aa1a6e5aa499080c
Lucas Kjaero <lucas@lucaskjaero.com>
2020-03-13 21:48:20 -0700

    Comment out K8s resources

--- infrastructure/terraform/kubernetes_cluster_infrastructure/main.tf
+++ infrastructure/terraform/kubernetes_cluster_infrastructure/main.tf
@@ -15,9 +15,9 @@ resource "digitalocean_project" "foreign_language_reader" {
   ]
 }
 
-data "digitalocean_kubernetes_cluster" "foreign_language_reader" {
-  name = var.cluster_name
-}
+# data "digitalocean_kubernetes_cluster" "foreign_language_reader" {
+#   name = var.cluster_name
+# }
 
 # Configure database
 
@@ -49,20 +49,20 @@ resource "digitalocean_database_db" "api_database" {
   name       = "foreign-language-reader"
 }
 
-resource "kubernetes_secret" "api_database_credentials" {
-  metadata {
-    name = "api-database-credentials"
-  }
-
-  data = {
-    username          = digitalocean_database_user.api_user.name
-    password          = digitalocean_database_user.api_user.password
-    host              = digitalocean_database_cluster.api_mysql.private_host
-    port              = digitalocean_database_cluster.api_mysql.port
-    database          = digitalocean_database_db.api_database.name
-    connection_string = "ecto://${digitalocean_database_user.api_user.name}:${digitalocean_database_user.api_user.password}@${digitalocean_database_cluster.api_mysql.private_host}:${digitalocean_database_cluster.api_mysql.port}/${digitalocean_database_db.api_database.name}"
-  }
-}
+# resource "kubernetes_secret" "api_database_credentials" {
+#   metadata {
+#     name = "api-database-credentials"
+#   }
+#
+#   data = {
+#     username          = digitalocean_database_user.api_user.name
+#     password          = digitalocean_database_user.api_user.password
+#     host              = digitalocean_database_cluster.api_mysql.private_host
+#     port              = digitalocean_database_cluster.api_mysql.port
+#     database          = digitalocean_database_db.api_database.name
+#     connection_string = "ecto://${digitalocean_database_user.api_user.name}:${digitalocean_database_user.api_user.password}@${digitalocean_database_cluster.api_mysql.private_host}:${digitalocean_database_cluster.api_mysql.port}/${digitalocean_database_db.api_database.name}"
+#   }
+# }
 
 # Cache for language service
 
@@ -85,60 +85,60 @@ resource "digitalocean_database_firewall" "allow_kubernetes_to_redis" {
   }
 }
 
-resource "kubernetes_secret" "language_service_cache_credentials" {
-  metadata {
-    name = "language-service-cache-credentials"
-  }
-
-  data = {
-    username          = digitalocean_database_cluster.language_service_cache.user
-    password          = digitalocean_database_cluster.language_service_cache.password
-    host              = digitalocean_database_cluster.language_service_cache.private_host
-    port              = digitalocean_database_cluster.language_service_cache.port
-    connection_string = "rediss://${digitalocean_database_cluster.language_service_cache.user}:${digitalocean_database_cluster.language_service_cache.password}@${digitalocean_database_cluster.language_service_cache.private_host}:${digitalocean_database_cluster.language_service_cache.port}/0"
-  }
-}
+# resource "kubernetes_secret" "language_service_cache_credentials" {
+#   metadata {
+#     name = "language-service-cache-credentials"
+#   }
+#
+#   data = {
+#     username          = digitalocean_database_cluster.language_service_cache.user
+#     password          = digitalocean_database_cluster.language_service_cache.password
+#     host              = digitalocean_database_cluster.language_service_cache.private_host
+#     port              = digitalocean_database_cluster.language_service_cache.port
+#     connection_string = "rediss://${digitalocean_database_cluster.language_service_cache.user}:${digitalocean_database_cluster.language_service_cache.password}@${digitalocean_database_cluster.language_service_cache.private_host}:${digitalocean_database_cluster.language_service_cache.port}/0"
+#   }
+# }
 
 # Configure networking
 
 # This is created by K8s and needs to be imported manually
 # Note that the ports are randomly assigned so you should update these to match what you import
-resource "digitalocean_loadbalancer" "foreign_language_reader" {
-  name   = "adfa0fdc0872441b18eff7df2ab9a1c7"
-  region = "sfo2"
-
-  forwarding_rule {
-    entry_port      = 80
-    entry_protocol  = "tcp"
-    target_port     = 32524
-    target_protocol = "tcp"
-  }
-
-  forwarding_rule {
-    entry_port      = 443
-    entry_protocol  = "tcp"
-    target_port     = 32723
-    target_protocol = "tcp"
-  }
-
-  healthcheck {
-    port                     = 32524
-    protocol                 = "tcp"
-    check_interval_seconds   = 3
-    response_timeout_seconds = 5
-  }
-}
-
-resource "digitalocean_record" "api" {
-  domain = var.domain_name
-  type   = "A"
-  name   = "api"
-  value  = digitalocean_loadbalancer.foreign_language_reader.ip
-}
-
-resource "digitalocean_record" "language" {
-  domain = var.domain_name
-  type   = "A"
-  name   = "language"
-  value  = digitalocean_loadbalancer.foreign_language_reader.ip
-}
+# resource "digitalocean_loadbalancer" "foreign_language_reader" {
+#   name   = "adfa0fdc0872441b18eff7df2ab9a1c7"
+#   region = "sfo2"
+#
+#   forwarding_rule {
+#     entry_port      = 80
+#     entry_protocol  = "tcp"
+#     target_port     = 32524
+#     target_protocol = "tcp"
+#   }
+#
+#   forwarding_rule {
+#     entry_port      = 443
+#     entry_protocol  = "tcp"
+#     target_port     = 32723
+#     target_protocol = "tcp"
+#   }
+#
+#   healthcheck {
+#     port                     = 32524
+#     protocol                 = "tcp"
+#     check_interval_seconds   = 3
+#     response_timeout_seconds = 5
+#   }
+# }
+#
+# resource "digitalocean_record" "api" {
+#   domain = var.domain_name
+#   type   = "A"
+#   name   = "api"
+#   value  = digitalocean_loadbalancer.foreign_language_reader.ip
+# }
+#
+# resource "digitalocean_record" "language" {
+#   domain = var.domain_name
+#   type   = "A"
+#   name   = "language"
+#   value  = digitalocean_loadbalancer.foreign_language_reader.ip
+# }
