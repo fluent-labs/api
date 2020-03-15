8249f43f50b6a8d0a247052dd407b0be0c05e98d
Lucas Kjaero-Zhang <lucaskjaero@users.noreply.github.com>
2020-03-15 11:21:17 -0700

    Terraform creates TLS certificate (#140)
    * Set up staging server for letsencrypt
    
    * Probably just make a valid cert
    
    * Set up the token
    
    * Read the manual I guess

--- infrastructure.tf
+++ infrastructure.tf
@@ -23,6 +23,11 @@ provider "digitalocean" {
   token = var.digitalocean_token
 }
 
+provider "acme" {
+  server_url = "https://acme-v02.api.letsencrypt.org/directory"
+}
+
+# Held here so that Helm and K8s providers can be initialized to work on this cluster
 resource "digitalocean_kubernetes_cluster" "foreign_language_reader" {
   name    = "foreign-language-reader"
   region  = "sfo2"
@@ -42,4 +47,28 @@ module "infrastructure" {
   source           = "./infrastructure/terraform"
   cluster_name     = digitalocean_kubernetes_cluster.foreign_language_reader.name
   test_environment = var.test_environment
+}
+
+# Section to create TLS certs
+# Put at this level so we don't have to pass the DO token around
+
+resource "tls_private_key" "tls_private_key" {
+  algorithm = "RSA"
+}
+
+resource "acme_registration" "reg" {
+  account_key_pem = tls_private_key.tls_private_key.private_key_pem
+  email_address   = "letsencrypt@lucaskjaerozhang.com"
+}
+
+resource "acme_certificate" "certificate" {
+  account_key_pem = acme_registration.reg.account_key_pem
+  common_name     = "*.foreignlanguagereader.com"
+
+  dns_challenge {
+    provider = "digitalocean"
+    config = {
+      DO_AUTH_TOKEN = var.digitalocean_token
+    }
+  }
 }
