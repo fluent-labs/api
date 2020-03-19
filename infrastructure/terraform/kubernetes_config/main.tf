b6c8d616df0467c922ad7f9ced94a91df6d41c3f
Lucas Kjaero <lucas@lucaskjaero.com>
2020-03-18 17:39:30 -0700

    Set up fluentd in helm

--- infrastructure/terraform/kubernetes_config/main.tf
+++ infrastructure/terraform/kubernetes_config/main.tf
@@ -119,3 +119,40 @@ resource "helm_release" "kibana" {
     kubernetes_secret.elastic_credentials
   ]
 }
+
+# Logging configuration
+
+resource "kubernetes_namespace" "logging" {
+  metadata {
+    annotations = {
+      name = "logging"
+    }
+
+    name = "logging"
+  }
+}
+
+resource "random_password" "fluent_elasticsearch_password" {
+  length      = 32
+  special     = false
+  min_numeric = 10
+}
+
+resource "helm_release" "fluentd_elasticsearch" {
+  name       = "fluentd"
+  repository = "https://kiwigrid.github.io"
+  chart      = "fluentd-elasticsearch"
+  version    = "6.2.2"
+  namespace  = "logging"
+
+  values = [file("${path.module}/fluentd.yml")]
+
+  set_sensitive {
+    name  = "elasticsearch.auth.password"
+    value = random_password.fluent_elasticsearch_password.result
+  }
+
+  depends_on = [
+    kubernetes_namespace.logging
+  ]
+}
