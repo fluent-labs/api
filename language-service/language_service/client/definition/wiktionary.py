07057c4578065790ddf5248e07056cb9c175d4bd
Lucas Kjaero <lucas@lucaskjaero.com>
2020-03-21 01:25:09 -0700

    Whoops

--- language-service/language_service/client/definition/wiktionary.py
+++ language-service/language_service/client/definition/wiktionary.py
@@ -24,7 +24,7 @@ class Wiktionary(DefinitionClient):
     def fetch_definitions(self, language, word):
         logger.info("Wiktionary - getting definitions in %s for %s" % (language, word))
         try:
-            response = self.fetch(word, language)
+            response = self.fetch(language, word)
         except Exception:
             logger.error("%s - Error fetching definition for %s" % (language, word))
             stacktrace = traceback.format_exc()
