e271c29c0a8d82e6e4ad85c5ac1755d6231cd9df
Lucas Kjaero <lucas@lucaskjaero.com>
2020-03-20 21:29:28 -0700

    Install zeppelin

--- infrastructure/terraform/kubernetes_config/main.tf
+++ infrastructure/terraform/kubernetes_config/main.tf
@@ -183,3 +183,15 @@ resource "helm_release" "spark" {
     kubernetes_namespace.content
   ]
 }
+
+resource "helm_release" "zeppelin" {
+  name       = "zeppelin"
+  repository = "https://kubernetes-charts.storage.googleapis.com"
+  chart      = "zeppelin"
+  version    = "1.1.1"
+  namespace  = "content"
+
+  depends_on = [
+    kubernetes_namespace.content
+  ]
+}
