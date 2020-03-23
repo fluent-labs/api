7da585d7d95486aadbf197fab7668443003d9762
Lucas Kjaero <lucas@lucaskjaero.com>
2020-03-23 00:10:02 -0700

    That was the most important part

--- language-service/language_service/client/definition/wiktionary.py
+++ language-service/language_service/client/definition/wiktionary.py
@@ -25,7 +25,7 @@ class Wiktionary(DefinitionClient):
         logger.info("Wiktionary - getting definitions in %s for %s" % (language, word))
         try:
             response = self.fetch(language, word)
-            logger.debug("Got response from wiktionary")
+            logger.debug("Got response from wiktionary: %s" % response)
         except Exception:
             logger.error("%s - Error fetching definition for %s" % (language, word))
             stacktrace = traceback.format_exc()
