e5df0a96160af9a389b230946020bfb8a938032d
Lucas Kjaero <lucas@lucaskjaero.com>
2020-03-24 12:08:38 -0700

    Api module

---
+++ infrastructure/terraform/api/main.tf
@@ -0,0 +1,95 @@
+data "digitalocean_kubernetes_cluster" "foreign_language_reader" {
+  name = var.cluster_name
+}
+
+resource "kubernetes_service" "api" {
+  metadata {
+    name = "api"
+  }
+  spec {
+    selector = {
+      app = "api"
+    }
+    port {
+      port = 4000
+    }
+    type = "ClusterIP"
+  }
+}
+
+resource "kubernetes_horizontal_pod_autoscaler" "example" {
+  metadata {
+    name = "api"
+  }
+  spec {
+    min_replicas = var.min_replicas
+    max_replicas = var.max_replicas
+    scale_target_ref {
+      kind = "Deployment"
+      name = "api"
+    }
+    target_cpu_utilization_percentage = 75
+  }
+}
+
+# Configure database
+
+resource "digitalocean_database_cluster" "api_mysql" {
+  name       = "foreign-language-reader"
+  engine     = "mysql"
+  version    = "8"
+  size       = "db-s-1vcpu-1gb"
+  region     = "sfo2"
+  node_count = 1
+}
+
+resource "digitalocean_database_firewall" "allow_kubernetes" {
+  cluster_id = digitalocean_database_cluster.api_mysql.id
+
+  rule {
+    type  = "k8s"
+    value = data.digitalocean_kubernetes_cluster.foreign_language_reader.id
+  }
+}
+
+resource "digitalocean_database_user" "api_user" {
+  cluster_id = digitalocean_database_cluster.api_mysql.id
+  name       = "api"
+}
+
+resource "digitalocean_database_db" "api_database" {
+  cluster_id = digitalocean_database_cluster.api_mysql.id
+  name       = "foreign-language-reader"
+}
+
+resource "kubernetes_secret" "api_database_credentials" {
+  metadata {
+    name = "api-database-credentials"
+  }
+
+  data = {
+    username          = digitalocean_database_user.api_user.name
+    password          = digitalocean_database_user.api_user.password
+    host              = digitalocean_database_cluster.api_mysql.private_host
+    port              = digitalocean_database_cluster.api_mysql.port
+    database          = digitalocean_database_db.api_database.name
+    connection_string = "ecto://${digitalocean_database_user.api_user.name}:${digitalocean_database_user.api_user.password}@${digitalocean_database_cluster.api_mysql.private_host}:${digitalocean_database_cluster.api_mysql.port}/${digitalocean_database_db.api_database.name}"
+  }
+}
+
+# Secret key base powers encryption at rest for the database
+
+resource "random_password" "api_secret_key_base" {
+  length  = 64
+  special = true
+}
+
+resource "kubernetes_secret" "api_secret_key_base" {
+  metadata {
+    name = "api-secret-key-base"
+  }
+
+  data = {
+    secret_key_base = random_password.api_secret_key_base.result
+  }
+}
