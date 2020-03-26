e56c0d5426ba2993b3885975d6ca5a09e92c3553
Lucas Kjaero-Zhang <lucaskjaero@users.noreply.github.com>
2020-03-25 19:05:07 -0700

    Set up service deployment in terraform (#159)
    * Just update the image
    
    * Api deployment in terraform
    
    * language service deployment in terraform
    
    * Yeah fair point
    
    * Sure

--- infrastructure/terraform/language_service/main.tf
+++ infrastructure/terraform/language_service/main.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 resource "kubernetes_service" "language_service" {
   metadata {
     name = "language-service"
@@ -25,6 +27,119 @@ resource "kubernetes_horizontal_pod_autoscaler" "language_service_autoscale" {
       name = "language-service"
     }
     target_cpu_utilization_percentage = 75
+  }
+}
+
+resource "kubernetes_deployment" "language_service" {
+  metadata {
+    name = "language-service"
+  }
+
+  spec {
+    selector {
+      match_labels = {
+        app = "language-service"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "language-service"
+        }
+      }
+
+      spec {
+        image_pull_secrets {
+          name = "aws-registry"
+        }
+
+        container {
+          image = "${data.aws_caller_identity.current.account_id}.dkr.ecr.us-west-2.amazonaws.com/foreign-language-reader-language-service:latest"
+          name  = "language-service"
+
+          env {
+            name = "AUTH_TOKEN"
+            value_from {
+              secret_key_ref {
+                name = "local-connection-token"
+                key  = "local_connection_token"
+              }
+            }
+          }
+
+          env {
+            name  = "ELASTICSEARCH_URL"
+            value = "http://elasticsearch-master.default.svc.cluster.local:9200"
+          }
+
+          env {
+            name = "ELASTICSEARCH_USERNAME"
+            value_from {
+              secret_key_ref {
+                name = "language-service-elastic-credentials"
+                key  = "username"
+              }
+            }
+          }
+
+          env {
+            name = "ELASTICSEARCH_PASSWORD"
+            value_from {
+              secret_key_ref {
+                name = "language-service-elastic-credentials"
+                key  = "password"
+              }
+            }
+          }
+
+          port {
+            container_port = 8000
+          }
+
+          resources {
+            limits {
+              memory = "1.2Gi"
+            }
+            requests {
+              memory = "800Mi"
+            }
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/health"
+              port = 4000
+            }
+
+            initial_delay_seconds = 30
+            period_seconds        = 10
+            timeout_seconds       = 5
+            failure_threshold     = 5
+          }
+
+          readiness_probe {
+            http_get {
+              path = "/health"
+              port = 4000
+            }
+
+            initial_delay_seconds = 30
+            period_seconds        = 10
+            timeout_seconds       = 5
+            failure_threshold     = 5
+          }
+        }
+      }
+    }
+  }
+
+  # This resource is to make sure the deployment exists
+  # Not blow away what's current for something that doesn't exist.
+  lifecycle {
+    ignore_changes = [
+      spec.0.template.0.spec.0.container.0.image,
+    ]
   }
 }
 
