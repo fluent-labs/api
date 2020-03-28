d1f0945a159559306d22fb459e46b905ebe346c1
Lucas Kjaero <lucas@lucaskjaero.com>
2020-03-28 12:47:20 -0700

    Don't overwhelm my cluster

--- infrastructure/terraform/monitoring/main.tf
+++ infrastructure/terraform/monitoring/main.tf
@@ -7,15 +7,3 @@ resource "kubernetes_namespace" "monitoring" {
     name = "monitoring"
   }
 }
-
-resource "helm_release" "prometheus" {
-  name       = "prometheus"
-  repository = "https://kubernetes-charts.storage.googleapis.com"
-  chart      = "prometheus-operator"
-  version    = "8.12.3"
-  namespace  = "monitoring"
-
-  depends_on = [
-    kubernetes_namespace.monitoring
-  ]
-}
