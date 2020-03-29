942f81991089215c9659a8a0f3a762a7b52a2b92
Lucas Kjaero <lucas@lucaskjaero.com>
2020-03-29 09:06:19 -0700

    Create metrics server for autoscaling services

--- infrastructure/terraform/monitoring/main.tf
+++ infrastructure/terraform/monitoring/main.tf
@@ -7,3 +7,11 @@ resource "kubernetes_namespace" "monitoring" {
     name = "monitoring"
   }
 }
+
+# Required for horizontal pod autoscaling to work
+resource "helm_release" "metrics_server" {
+  name       = "metrics-server"
+  repository = "https://kubernetes-charts.storage.googleapis.com"
+  chart      = "metrics-server"
+  version    = "2.10.1"
+}
