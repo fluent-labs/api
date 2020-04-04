6883ae1be697c7b5a3a2f415439c651bd8225940
Lucas Kjaero <lucas@lucaskjaero.com>
2020-04-04 12:14:49 -0700

    API ready for environments

--- infrastructure/terraform/api/main.tf
+++ infrastructure/terraform/api/main.tf
@@ -8,6 +8,10 @@ data "digitalocean_kubernetes_cluster" "foreign_language_reader" {
   name = var.cluster_name
 }
 
+data "digitalocean_database_cluster" "api_mysql" {
+  name = var.database_name
+}
+
 resource "kubernetes_service" "api" {
   metadata {
     name = "api"
@@ -192,8 +196,6 @@ resource "kubernetes_deployment" "api" {
 
   # The deployment will not come up without the database connection
   depends_on = [
-    digitalocean_database_cluster.api_mysql,
-    digitalocean_database_firewall.allow_kubernetes,
     digitalocean_database_user.api_user,
     digitalocean_database_db.api_database,
     kubernetes_secret.api_database_credentials
@@ -202,32 +204,14 @@ resource "kubernetes_deployment" "api" {
 
 # Configure database
 
-resource "digitalocean_database_cluster" "api_mysql" {
-  name       = "foreign-language-reader"
-  engine     = "mysql"
-  version    = "8"
-  size       = "db-s-1vcpu-1gb"
-  region     = "sfo2"
-  node_count = 1
-}
-
-resource "digitalocean_database_firewall" "allow_kubernetes" {
-  cluster_id = digitalocean_database_cluster.api_mysql.id
-
-  rule {
-    type  = "k8s"
-    value = data.digitalocean_kubernetes_cluster.foreign_language_reader.id
-  }
-}
-
 resource "digitalocean_database_user" "api_user" {
-  cluster_id = digitalocean_database_cluster.api_mysql.id
-  name       = "api"
+  cluster_id = data.digitalocean_database_cluster.api_mysql.id
+  name       = "api-${var.env}"
 }
 
 resource "digitalocean_database_db" "api_database" {
-  cluster_id = digitalocean_database_cluster.api_mysql.id
-  name       = "foreign-language-reader"
+  cluster_id = data.digitalocean_database_cluster.api_mysql.id
+  name       = "foreign-language-reader-${var.env}"
 }
 
 resource "kubernetes_secret" "api_database_credentials" {
@@ -238,10 +222,10 @@ resource "kubernetes_secret" "api_database_credentials" {
   data = {
     username          = digitalocean_database_user.api_user.name
     password          = digitalocean_database_user.api_user.password
-    host              = digitalocean_database_cluster.api_mysql.private_host
-    port              = digitalocean_database_cluster.api_mysql.port
+    host              = data.digitalocean_database_cluster.api_mysql.private_host
+    port              = data.digitalocean_database_cluster.api_mysql.port
     database          = digitalocean_database_db.api_database.name
-    connection_string = "ecto://${digitalocean_database_user.api_user.name}:${digitalocean_database_user.api_user.password}@${digitalocean_database_cluster.api_mysql.private_host}:${digitalocean_database_cluster.api_mysql.port}/${digitalocean_database_db.api_database.name}"
+    connection_string = "ecto://${digitalocean_database_user.api_user.name}:${digitalocean_database_user.api_user.password}@${data.digitalocean_database_cluster.api_mysql.private_host}:${data.digitalocean_database_cluster.api_mysql.port}/${digitalocean_database_db.api_database.name}"
   }
 }
 
