6208aeaf942df8fc03fca3c27db7507184b7ecb8
Lucas Kjaero <lucas@lucaskjaero.com>
2020-06-08 18:31:47 -0700

    Readiness can handle its own logic

--- api-v2/app/com/foreignlanguagereader/api/dto/v1/Readiness.scala
+++ api-v2/app/com/foreignlanguagereader/api/dto/v1/Readiness.scala
@@ -1,20 +1,54 @@
 package com.foreignlanguagereader.api.dto.v1
-
-import com.foreignlanguagereader.api.ReadinessStatus.ReadinessStatus
-import play.api.libs.json.{Format, Json}
+import com.foreignlanguagereader.api.dto.v1.ReadinessService.ReadinessService
+import com.foreignlanguagereader.api.dto.v1.ReadinessStatus.ReadinessStatus
+import play.api.libs.json._
 
 /**
   *
-  * @param overall  Overall service status
   * @param database Whether service can connect to database to get user data
   * @param content  Whether service can connect to elasticsearch to get language content
   */
-case class Readiness(overall: ReadinessStatus,
-                     database: ReadinessStatus,
+case class Readiness(database: ReadinessStatus,
                      content: ReadinessStatus,
-                     languageService: ReadinessStatus)
+                     languageService: ReadinessStatus) {
+  def overall: ReadinessStatus = {
+    val statuses = List(database, content, languageService)
+    if (statuses.forall(_.eq(ReadinessStatus.UP))) ReadinessStatus.UP
+    else if (statuses.forall(_.eq(ReadinessStatus.DOWN)))
+      ReadinessStatus.DOWN
+    else ReadinessStatus.DEGRADED
+  }
+}
 
 object Readiness {
   // Allows readiness to be serialized to JSON
   implicit val format: Format[Readiness] = Json.format
+
+  def fromMAP(statuses: Map[ReadinessService, ReadinessStatus]): Readiness =
+    Readiness(
+      statuses.getOrElse(ReadinessService.DATABASE, ReadinessStatus.DOWN),
+      statuses.getOrElse(ReadinessService.ELASTICSEARCH, ReadinessStatus.DOWN),
+      statuses
+        .getOrElse(ReadinessService.LANGUAGE_SERVICE, ReadinessStatus.DOWN)
+    )
+}
+
+object ReadinessStatus extends Enumeration {
+  type ReadinessStatus = Value
+  val UP, DOWN, DEGRADED = Value
+
+  implicit val readinessStatusFormat: Format[ReadinessStatus] =
+    new Format[ReadinessStatus] {
+      def reads(json: JsValue) =
+        JsSuccess(ReadinessStatus.withName(json.as[String]))
+      def writes(status: ReadinessStatus.ReadinessStatus) =
+        JsString(status.toString)
+    }
+}
+
+object ReadinessService extends Enumeration {
+  type ReadinessService = Value
+  val DATABASE: Value = Value("database")
+  val ELASTICSEARCH: Value = Value("elasticsearch")
+  val LANGUAGE_SERVICE: Value = Value("languageservice")
 }
