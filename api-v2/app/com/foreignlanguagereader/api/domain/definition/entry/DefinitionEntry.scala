4261b887b09891aab8f0068dbc25aae729341c87
Lucas Kjaero <lucas@lucaskjaero.com>
2020-06-08 18:28:36 -0700

    Push serialization logic into the domain types

--- api-v2/app/com/foreignlanguagereader/api/domain/definition/entry/DefinitionEntry.scala
+++ api-v2/app/com/foreignlanguagereader/api/domain/definition/entry/DefinitionEntry.scala
@@ -1,36 +1,22 @@
 package com.foreignlanguagereader.api.domain.definition.entry
 
 import com.foreignlanguagereader.api.Language.Language
-import com.foreignlanguagereader.api.domain.definition.combined.Definition
-import com.foreignlanguagereader.api.domain.definition.entry.DefinitionSource.DefinitionSource
-import play.api.libs.json.{Format, JsString, JsSuccess, JsValue}
-
-abstract class DefinitionEntry(val subdefinitions: List[String],
-                               // These fields are needed for elasticsearch lookup
-                               // But do not need to be presented to the user.
-                               val language: Language,
-                               val source: DefinitionSource,
-                               val token: String) {}
-
-object DefinitionEntry {
-  // This exists to bring each type of definition entry into scope
-  // That way the client can handle each different type of entry without knowing how to handle it
-  // And still return a uniform model to the service
-  implicit def convertToDefinition(
-    definitionEntry: DefinitionEntry
-  ): Definition = definitionEntry match {
-    case w: WiktionaryDefinitionEntry => w
-    case c: CEDICTDefinitionEntry     => c
-  }
-  implicit def convertToSeqOfDefinitions(
-    definitions: Seq[DefinitionEntry]
-  ): Seq[Definition] = definitions.map(x => convertToDefinition(x))
+import com.foreignlanguagereader.api.domain.definition.combined.{
+  ChineseDefinition,
+  Definition
 }
+import com.foreignlanguagereader.api.domain.definition.entry.DefinitionSource.DefinitionSource
+import com.foreignlanguagereader.api.{HSKLevel, Language}
+import com.sksamuel.elastic4s.{Hit, HitReader}
+import play.api.libs.json._
+
+import scala.util.{Success, Try}
 
 object DefinitionSource extends Enumeration {
   type DefinitionSource = Value
   val WIKTIONARY, CEDICT, MULTIPLE = Value
 
+  // Makes sure we can serialize and deserialize this to JSON
   implicit val sourceFormat: Format[DefinitionSource] =
     new Format[DefinitionSource] {
       def reads(json: JsValue) =
@@ -38,4 +24,136 @@ object DefinitionSource extends Enumeration {
       def writes(source: DefinitionSource.DefinitionSource) =
         JsString(source.toString)
     }
+}
+
+// Companion object is at bottom of file
+sealed trait DefinitionEntry {
+  val subdefinitions: List[String]
+  val language: Language
+  val source: DefinitionSource
+  val token: String
+}
+
+case class CEDICTDefinitionEntry(override val subdefinitions: List[String],
+                                 pinyin: String,
+                                 simplified: String,
+                                 traditional: String,
+                                 override val token: String)
+    extends DefinitionEntry {
+  val language: Language = Language.CHINESE
+  val source: DefinitionSource = DefinitionSource.CEDICT
+}
+
+object CEDICTDefinitionEntry {
+  // Allows serializing and deserializing in json
+  implicit val reads: Reads[CEDICTDefinitionEntry] =
+    Json.reads[CEDICTDefinitionEntry]
+  implicit val readsSeq: Reads[Seq[CEDICTDefinitionEntry]] =
+    Reads.seq(reads)
+
+  implicit def convertToDefinition(
+    c: CEDICTDefinitionEntry
+  ): ChineseDefinition =
+    ChineseDefinition(
+      c.subdefinitions,
+      tag = "",
+      examples = List(),
+      c.pinyin,
+      c.simplified,
+      c.traditional,
+      HSKLevel.NONE,
+      DefinitionSource.CEDICT,
+      c.token
+    )
+
+  // Lets us pull this from elasticsearch
+  implicit object CEDICTHitReader extends HitReader[CEDICTDefinitionEntry] {
+    override def read(hit: Hit): Try[CEDICTDefinitionEntry] = {
+      val source = hit.sourceAsMap
+      Success(
+        CEDICTDefinitionEntry(
+          source("subdefinitions").asInstanceOf[List[String]],
+          source("pinyin").toString,
+          source("simplified").toString,
+          source("traditional").toString,
+          source("token").toString
+        )
+      )
+    }
+  }
+}
+
+case class WiktionaryDefinitionEntry(override val subdefinitions: List[String],
+                                     tag: String,
+                                     examples: List[String],
+                                     override val language: Language,
+                                     override val token: String)
+    extends DefinitionEntry {
+  val source: DefinitionSource = DefinitionSource.CEDICT
+}
+
+object WiktionaryDefinitionEntry {
+  // Allows serializing and deserializing in json
+  implicit val reads: Reads[WiktionaryDefinitionEntry] =
+    Json.reads[WiktionaryDefinitionEntry]
+  implicit val readsSeq: Reads[Seq[WiktionaryDefinitionEntry]] =
+    Reads.seq(reads)
+
+  implicit def convertToDefinition(w: WiktionaryDefinitionEntry): Definition =
+    Definition(
+      w.subdefinitions,
+      w.tag,
+      w.examples,
+      w.language,
+      DefinitionSource.WIKTIONARY,
+      w.token
+    )
+
+  // Used for elasticsearch
+  implicit object WiktionaryHitReader
+      extends HitReader[WiktionaryDefinitionEntry] {
+    override def read(hit: Hit): Try[WiktionaryDefinitionEntry] = {
+      val source = hit.sourceAsMap
+      Success(
+        WiktionaryDefinitionEntry(
+          source("subdefinitions").asInstanceOf[List[String]],
+          source("tag").toString,
+          source("examples").toString.asInstanceOf[List[String]],
+          Language.withName(source("language").toString),
+          source("token").toString
+        )
+      )
+    }
+  }
+}
+
+object DefinitionEntry {
+  // Deserializing from JSON
+  implicit val reads: Reads[DefinitionEntry] =
+    Json.reads[DefinitionEntry]
+  implicit val readsSeq: Reads[Seq[DefinitionEntry]] =
+    Reads.seq(reads)
+
+  // Mapping DefinitionEntry => Definition
+  implicit def convertToDefinition(d: DefinitionEntry): Definition =
+    d match {
+      case c: CEDICTDefinitionEntry     => c
+      case w: WiktionaryDefinitionEntry => w
+    }
+  implicit def convertToSeqOfDefinitions(
+    d: Seq[DefinitionEntry]
+  ): Seq[Definition] = d.map(e => convertToDefinition(e))
+
+  // For elasticsearch - this does dynamic dispatch based on the type
+  implicit object DefinitionEntryHitReader extends HitReader[DefinitionEntry] {
+    override def read(hit: Hit): Try[DefinitionEntry] = {
+      val source = hit.sourceAsMap
+      source("source") match {
+        case DefinitionSource.CEDICT =>
+          CEDICTDefinitionEntry.CEDICTHitReader.read(hit)
+        case DefinitionSource.WIKTIONARY =>
+          WiktionaryDefinitionEntry.WiktionaryHitReader.read(hit)
+      }
+    }
+  }
 }
--- api-v2/app/com/foreignlanguagereader/api/dto/v1/Word.scala
+++ api-v2/app/com/foreignlanguagereader/api/dto/v1/Word.scala
@@ -1,9 +1,14 @@
 package com.foreignlanguagereader.api.dto.v1
 
-import play.api.libs.json.{Format, Json}
+import com.foreignlanguagereader.api.domain.definition.entry.WiktionaryDefinitionEntry
+import play.api.libs.json.{Format, Json, Reads}
 
 case class Word(token: String, tag: String, lemma: String)
 
 object Word {
   implicit val format: Format[Word] = Json.format
+  implicit val reads: Reads[Word] =
+    Json.reads[Word]
+  implicit val readsSeq: Reads[Seq[Word]] =
+    Reads.seq(reads)
 }
