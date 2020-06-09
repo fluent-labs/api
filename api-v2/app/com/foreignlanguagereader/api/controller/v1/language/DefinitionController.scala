04aad5f09fbf1ae8e4e3f2ff8d46e7aa1cc447b4
Lucas Kjaero <lucas@lucaskjaero.com>
2020-06-08 22:45:27 -0700

    Wire up definition controller

--- api-v2/app/com/foreignlanguagereader/api/controller/v1/language/DefinitionController.scala
+++ api-v2/app/com/foreignlanguagereader/api/controller/v1/language/DefinitionController.scala
@@ -1,5 +1,6 @@
 package com.foreignlanguagereader.api.controller.v1.language
 
+import com.foreignlanguagereader.api.domain.Language
 import com.foreignlanguagereader.api.domain.Language.Language
 import com.foreignlanguagereader.api.domain.definition.combined.Definition
 import com.foreignlanguagereader.api.service.DefinitionService
@@ -8,7 +9,7 @@ import play.api.Logger
 import play.api.libs.json.Json
 import play.api.mvc._
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class DefinitionController @Inject()(
@@ -18,27 +19,36 @@ class DefinitionController @Inject()(
 ) extends BaseController {
   val logger: Logger = Logger(this.getClass)
 
-  def definition(wordLanguage: Language,
-                 definitionLanguage: Language,
-                 word: String): Action[AnyContent] = Action.async {
-    implicit request: Request[AnyContent] =>
-      definitionService
-        .getDefinition(wordLanguage, definitionLanguage, word)
-        .map {
-          case None =>
-            NotFound(s"Definition for $word in $language not found")
-          case Some(definitions) =>
-            Ok(
-              Json.toJson(
-                Definition.definitionListToDefinitionDTOList(definitions)
-              )
-            )
-        }
-        .recover(error => {
-          val message =
-            s"Failed to get definitions for $word in $language: ${error.getMessage}"
-          logger.error(message, error)
-          InternalServerError(message)
-        })
+  def definition(wordLanguage: Language, word: String): Action[AnyContent] =
+    Action.async {
+      getDefinitions(wordLanguage, Language.ENGLISH, word)
+    }
+
+  def definitionIn(wordLanguage: Language,
+                   definitionLanguage: Language,
+                   word: String): Action[AnyContent] = Action.async {
+    getDefinitions(wordLanguage, definitionLanguage, word)
+  }
+
+  def getDefinitions(wordLanguage: Language,
+                     definitionLanguage: Language,
+                     word: String): Future[Result] = {
+    definitionService
+      .getDefinition(wordLanguage, definitionLanguage, word)
+      .map {
+        case None =>
+          NotFound(s"Definition for $word in $language not found")
+        case Some(definitions) =>
+          Ok(
+            Json
+              .toJson(Definition.definitionListToDefinitionDTOList(definitions))
+          )
+      }
+      .recover(error => {
+        val message =
+          s"Failed to get definitions for $word in $language: ${error.getMessage}"
+        logger.error(message, error)
+        InternalServerError(message)
+      })
   }
 }
--- api-v2/app/com/foreignlanguagereader/api/domain/Language.scala
+++ api-v2/app/com/foreignlanguagereader/api/domain/Language.scala
@@ -1,13 +1,27 @@
 package com.foreignlanguagereader.api.domain
 
 import play.api.libs.json._
+import play.api.mvc.PathBindable
 
 object Language extends Enumeration {
   type Language = Value
   val CHINESE, ENGLISH, SPANISH = Value
 
+  def isLanguage(s: String): Boolean = values.exists(_.toString == s)
+
   implicit val languageFormat: Format[Language] = new Format[Language] {
     def reads(json: JsValue) = JsSuccess(Language.withName(json.as[String]))
     def writes(language: Language.Language) = JsString(language.toString)
   }
+
+  implicit def pathBinder: PathBindable[Language] =
+    new PathBindable[Language] {
+      override def bind(key: String, value: String): Either[String, Language] =
+        value match {
+          case language if Language.isLanguage(language) =>
+            Right(Language.withName(value))
+          case _ => Left(value)
+        }
+      override def unbind(key: String, value: Language): String = value.toString
+    }
 }
