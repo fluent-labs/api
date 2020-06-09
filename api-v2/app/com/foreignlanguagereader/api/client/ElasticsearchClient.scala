414e987fa56f11cb0a1c19f5c97efb6bb79d44d8
Lucas Kjaero <lucas@lucaskjaero.com>
2020-06-08 18:27:26 -0700

    Improved clients

--- api-v2/app/com/foreignlanguagereader/api/client/ElasticsearchClient.scala
+++ api-v2/app/com/foreignlanguagereader/api/client/ElasticsearchClient.scala
@@ -1,13 +1,91 @@
 package com.foreignlanguagereader.api.client
 
-import com.foreignlanguagereader.api.Language.Language
-import com.foreignlanguagereader.api.ReadinessStatus.ReadinessStatus
-import com.foreignlanguagereader.api.domain.definition.entry.DefinitionEntry
+import java.util.concurrent.{TimeUnit, TimeoutException}
 
-class ElasticsearchClient {
-  def checkConnection: ReadinessStatus = ???
+import akka.actor.ActorSystem
+import com.foreignlanguagereader.api.Language.Language
+import com.foreignlanguagereader.api.domain.definition.entry.DefinitionEntry
+import com.foreignlanguagereader.api.dto.v1.ReadinessStatus
+import com.foreignlanguagereader.api.dto.v1.ReadinessStatus.ReadinessStatus
+import com.sksamuel.elastic4s.http.JavaClient
+import com.sksamuel.elastic4s.{
+  ElasticClient,
+  ElasticProperties,
+  RequestFailure,
+  RequestSuccess
+}
+import javax.inject.Inject
+import play.api.Logger
+
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.duration.Duration
+
+class ElasticsearchClient @Inject()(val system: ActorSystem) {
+  val logger: Logger = Logger(this.getClass)
+
+  // What's with all the awaits? elastic4s does not implement connection timeouts.
+  // Instead, we need to implement them ourselves.
+  // Not the end of the world, we would have been blocking here anyway since it is IO
+  // We use this thread pool to keep it out of the main server threads
+  implicit val myExecutionContext: ExecutionContext =
+    system.dispatchers.lookup("elasticsearch-context")
+
+  val elasticSearchUrl: String = "elasticSearchURL"
+  val elasticSearchTimeout = Duration(5, TimeUnit.SECONDS)
+  val client = ElasticClient(JavaClient(ElasticProperties(elasticSearchUrl)))
+
+  val definitionsIndex = "definitions"
+
+  import com.sksamuel.elastic4s.ElasticDsl._
+
+  def checkConnection(timeout: Duration): ReadinessStatus =
+    try {
+      Await.result(client.execute(indexExists(definitionsIndex)), timeout) match {
+        case RequestSuccess(_, _, _, result) if result.exists =>
+          ReadinessStatus.UP
+        case RequestSuccess(_, _, _, _) =>
+          logger.error(
+            s"Error connecting to elasticsearch: index $definitionsIndex does not exist"
+          )
+          ReadinessStatus.DOWN
+        case RequestFailure(_, _, _, error) =>
+          logger.error(
+            s"Error connecting to elasticsearch: ${error.reason}",
+            error.asException
+          )
+          ReadinessStatus.DOWN
+      }
+    } catch {
+      case e: Exception =>
+        logger
+          .error(s"Failed to connect to elasticsearch: ${e.getMessage}", e)
+        ReadinessStatus.DOWN
+    }
 
   def getDefinition(wordLanguage: Language,
                     definitionLanguage: Language,
-                    word: String): Option[Seq[DefinitionEntry]] = ???
+                    word: String): Option[Seq[DefinitionEntry]] = {
+    Await.result(
+      client.execute({
+        search(definitionsIndex).query(
+          boolQuery()
+            .must(
+              matchQuery("language", wordLanguage.toString),
+              matchQuery("token", word)
+            )
+        )
+      }),
+      elasticSearchTimeout
+    ) match {
+      case RequestSuccess(_, _, _, result) =>
+        val results = result.hits.hits.map(_.to[DefinitionEntry])
+        if (results.nonEmpty) Some(results.toIndexedSeq) else None
+      case RequestFailure(_, _, _, error) =>
+        logger.error(
+          s"Error fetching definitions from elasticsearch: ${error.reason}",
+          error.asException
+        )
+        throw error.asException
+    }
+  }
 }
--- api-v2/app/com/foreignlanguagereader/api/client/LanguageServiceClient.scala
+++ api-v2/app/com/foreignlanguagereader/api/client/LanguageServiceClient.scala
@@ -2,76 +2,105 @@ package com.foreignlanguagereader.api.client
 
 import java.util.concurrent.TimeUnit
 
+import akka.actor.ActorSystem
 import com.foreignlanguagereader.api.Language.Language
-import com.foreignlanguagereader.api.ReadinessStatus
-import com.foreignlanguagereader.api.ReadinessStatus.ReadinessStatus
-import com.foreignlanguagereader.api.domain.definition.entry.{
-  CEDICTDefinitionEntry,
-  DefinitionEntry,
-  WiktionaryDefinitionEntry
-}
-import com.foreignlanguagereader.api.dto.v1.Word
+import com.foreignlanguagereader.api.domain.definition.entry.DefinitionEntry
+import com.foreignlanguagereader.api.dto.v1.ReadinessStatus.ReadinessStatus
+import com.foreignlanguagereader.api.dto.v1.{ReadinessStatus, Word}
 import javax.inject.Inject
+import play.api.Logger
 import play.api.http.Status.OK
-import play.api.libs.json.{JsSuccess, Json, Reads}
+import play.api.libs.json.{JsError, JsSuccess}
 import play.api.libs.ws.WSClient
 
 import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, ExecutionContext}
+import scala.concurrent.{ExecutionContext, Future}
 
 class LanguageServiceClient @Inject()(val ws: WSClient,
-                                      implicit val ec: ExecutionContext) {
+                                      val system: ActorSystem) {
   val languageServiceBaseUrl = "languageServiceBaseUrl"
   val languageServiceTimeout = Duration(5, TimeUnit.SECONDS)
 
-  // Tells the JSON library that it can handle these types
-  implicit val cedictDefinitionReads: Reads[CEDICTDefinitionEntry] =
-    Json.reads[CEDICTDefinitionEntry]
-  implicit val cedictDefinitionListReads: Reads[Seq[CEDICTDefinitionEntry]] =
-    Reads.seq(cedictDefinitionReads)
-  implicit val wiktionaryDefinitionReads: Reads[WiktionaryDefinitionEntry] =
-    Json.reads[WiktionaryDefinitionEntry]
-  implicit val wiktionaryDefinitionListReads
-    : Reads[Seq[WiktionaryDefinitionEntry]] =
-    Reads.seq(wiktionaryDefinitionReads)
-  implicit val documentReads: Reads[Word] = Json.reads[Word]
-  implicit val documentListReads: Reads[Seq[Word]] =
-    Reads.seq(documentReads)
+  val logger: Logger = Logger(this.getClass)
+
+  implicit val myExecutionContext: ExecutionContext =
+    system.dispatchers.lookup("language-service-context")
 
   // Used for the health check. Just makes sure we can connect to language service
-  def checkConnection: ReadinessStatus = {
-    val request = ws.url(s"$languageServiceBaseUrl/health").get()
-    Await.result(request, languageServiceTimeout).status match {
-      case OK => ReadinessStatus.UP
-      case _  => ReadinessStatus.DOWN
-    }
+  def checkConnection(timeout: Duration): Future[ReadinessStatus] = {
+    ws.url(s"$languageServiceBaseUrl/health")
+      .withRequestTimeout(timeout)
+      .get()
+      .map(
+        result =>
+          result.status match {
+            case OK => ReadinessStatus.UP
+            case _ =>
+              logger
+                .error(s"Error connecting to language service: ${result.body}")
+              ReadinessStatus.DOWN
+        }
+      )
+      .recover {
+        case e: Exception =>
+          logger
+            .error(s"Failed to connect to language service: ${e.getMessage}", e)
+          ReadinessStatus.DOWN
+      }
   }
 
   def getDefinition(wordLanguage: Language,
                     _definitionLanguage: Language,
-                    word: String): Option[Seq[DefinitionEntry]] = {
-    val request =
-      ws.url(s"$languageServiceBaseUrl/v1/definition/$language/$word").get()
-    Await
-      .result(request, languageServiceTimeout)
-      .json
-      .validate[Seq[Word]] match {
-      case d: JsSuccess[Seq[DefinitionEntry]] => Some(d.get)
-      case _                                  => None
-    }
+                    word: String): Future[Option[Seq[DefinitionEntry]]] = {
+    ws.url(s"$languageServiceBaseUrl/v1/definition/$language/$word")
+      .withRequestTimeout(languageServiceTimeout)
+      .get()
+      .map(
+        response =>
+          response.json.validate[Seq[DefinitionEntry]] match {
+            case JsSuccess(result, _) => Some(result)
+            case JsError(errors) =>
+              logger.error(
+                s"Failed to parse definition in $language for word $word from language service: $errors"
+              )
+              None
+        }
+      )
+      .recover {
+        case e: Exception =>
+          logger.error(
+            s"Failed to get definitions in $language for word $word from language service: ${e.getMessage}",
+            e
+          )
+          throw e
+      }
   }
 
   def getWordsForDocument(documentLanguage: Language,
-                          document: String): Option[Seq[Word]] = {
-    val request = ws
-      .url(s"$languageServiceBaseUrl/v1/tagging/$documentLanguage/document")
+                          document: String): Future[Seq[Word]] = {
+    ws.url(s"$languageServiceBaseUrl/v1/tagging/$documentLanguage/document")
+      .withRequestTimeout(languageServiceTimeout)
       .post(document)
-    Await
-      .result(request, languageServiceTimeout)
-      .json
-      .validate(documentListReads) match {
-      case w: JsSuccess[Seq[Word]] => Some(w.get)
-      case _                       => None
-    }
+      .map(
+        response =>
+          response.json.validate[Seq[Word]] match {
+            case JsSuccess(result, _) => result
+            case JsError(errors) =>
+              logger.error(
+                s"Failed to parse words in $language for document $document from language service: $errors"
+              )
+              throw new IllegalArgumentException(
+                s"Failed to parse words in $language for document $document from language service: $errors"
+              )
+        }
+      )
+      .recover {
+        case e: Exception =>
+          logger.error(
+            s"Failed to get words in $language for document $document from language service: ${e.getMessage}",
+            e
+          )
+          throw e
+      }
   }
 }
