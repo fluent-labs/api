261a31e461c13ae0ead078e89eb5433c68918178
Lucas Kjaero <lucas@lucaskjaero.com>
2020-06-09 21:46:35 -0700

    Document controller

---
+++ api-v2/app/com/foreignlanguagereader/api/controller/v1/language/DocumentController.scala
@@ -0,0 +1,45 @@
+package com.foreignlanguagereader.api.controller.v1.language
+
+import com.foreignlanguagereader.api.domain.Language.Language
+import com.foreignlanguagereader.api.dto.v1.document.DocumentRequest
+import com.foreignlanguagereader.api.service.DocumentService
+import javax.inject._
+import play.api.Logger
+import play.api.libs.json.{JsError, JsSuccess, JsValue, Json}
+import play.api.mvc._
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class DocumentController @Inject()(
+  val controllerComponents: ControllerComponents,
+  val documentService: DocumentService,
+  implicit val ec: ExecutionContext
+) extends BaseController {
+  val logger: Logger = Logger(this.getClass)
+
+  def definition(wordLanguage: Language): Action[AnyContent] =
+    Action.async(parse.json) { request =>
+      request.body.validate[DocumentRequest] match {
+        case JsSuccess(documentRequest: DocumentRequest, _) =>
+          documentService
+            .getWordsForDocument(wordLanguage, documentRequest.text)
+            .map(words => {
+              Ok(Json.toJson(words))
+            })
+            .recover(error => {
+              val message =
+                s"Failed to get words in $wordLanguage: ${error.getMessage}"
+              logger.error(message, error)
+              InternalServerError(message)
+            })
+        case JsError(errors) =>
+          logger.error(
+            s"Invalid request body given to document service: $errors"
+          )
+          Future {
+            BadRequest("Invalid request body, please try again")
+          }
+      }
+    }
+}
