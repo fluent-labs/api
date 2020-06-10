6e2054ae53c22a2456311cfc3be2131634526891
Lucas Kjaero <lucas@lucaskjaero.com>
2020-06-09 20:34:35 -0700

    DefinitionDTO as a trait

--- api-v2/app/com/foreignlanguagereader/api/dto/v1/definition/DefinitionDTO.scala
+++ api-v2/app/com/foreignlanguagereader/api/dto/v1/definition/DefinitionDTO.scala
@@ -1,6 +1,6 @@
 package com.foreignlanguagereader.api.dto.v1.definition
 
-import play.api.libs.json.{Format, Json}
+import play.api.libs.json.{Format, JsError, JsResult, JsValue}
 
 trait DefinitionDTO {
   val subdefinitions: List[String]
@@ -8,7 +8,16 @@ trait DefinitionDTO {
   val examples: List[String]
 }
 object DefinitionDTO {
-  implicit val format: Format[GenericDefinitionDTO] = Json.format
+  implicit val formatDefinitionDTO: Format[DefinitionDTO] =
+    new Format[DefinitionDTO] {
+      override def reads(json: JsValue): JsResult[DefinitionDTO] =
+        JsError("We are not supposed to read definitions in")
+      override def writes(o: DefinitionDTO): JsValue = o match {
+        case c: ChineseDefinitionDTO => ChineseDefinitionDTO.format.writes(c)
+        case g: GenericDefinitionDTO =>
+          GenericDefinitionDTO.format.writes(g)
+      }
+    }
 
   // Default to generic
   def apply(subdefinitions: List[String], tag: String, examples: List[String]) =
--- api-v2/app/com/foreignlanguagereader/api/dto/v1/definition/GenericDefinitionDTO.scala
+++ api-v2/app/com/foreignlanguagereader/api/dto/v1/definition/GenericDefinitionDTO.scala
@@ -1,6 +1,11 @@
 package com.foreignlanguagereader.api.dto.v1.definition
 
+import play.api.libs.json.{Format, Json}
+
 case class GenericDefinitionDTO(subdefinitions: List[String],
                                 tag: String,
                                 examples: List[String])
     extends DefinitionDTO
+object GenericDefinitionDTO {
+  implicit val format: Format[GenericDefinitionDTO] = Json.format
+}
