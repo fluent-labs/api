e740067d1b8948338dac182ab75fa60f1cd47152
Lucas Kjaero <lucas@lucaskjaero.com>
2020-06-14 17:44:52 -0700

    Use the type

--- api-v2/app/com/foreignlanguagereader/api/dto/v1/definition/ChineseDefinitionDTO.scala
+++ api-v2/app/com/foreignlanguagereader/api/dto/v1/definition/ChineseDefinitionDTO.scala
@@ -1,17 +1,15 @@
 package com.foreignlanguagereader.api.dto.v1.definition
 
+import com.foreignlanguagereader.api.domain.definition.combined.ChinesePronunciation
 import com.foreignlanguagereader.api.domain.definition.combined.HskLevel.HSKLevel
 import play.api.libs.json.{Format, Json}
 
 case class ChineseDefinitionDTO(subdefinitions: List[String],
                                 tag: String,
                                 examples: List[String],
-                                pinyin: String = "",
                                 simplified: String = "",
                                 traditional: String = "",
-                                ipa: String,
-                                zhuyin: String,
-                                wadeGiles: String,
+                                pronunciation: ChinesePronunciation,
                                 hsk: HSKLevel)
     extends DefinitionDTO
 
