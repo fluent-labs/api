d43ef3630101e964615b05c5699020ddc3a7af35
Lucas Kjaero <lucas@lucaskjaero.com>
2020-06-15 18:57:22 -0700

    Finish definition service tests

--- api-v2/test/com/foreignlanguagereader/api/service/definition/DefinitionServiceTest.scala
+++ api-v2/test/com/foreignlanguagereader/api/service/definition/DefinitionServiceTest.scala
@@ -1,7 +1,11 @@
 package com.foreignlanguagereader.api.service.definition
 
 import com.foreignlanguagereader.api.domain.Language
-import com.foreignlanguagereader.api.domain.definition.combined.ChineseDefinition
+import com.foreignlanguagereader.api.domain.definition.combined.{
+  ChineseDefinition,
+  Definition,
+  GenericDefinition
+}
 import com.foreignlanguagereader.api.domain.definition.entry.DefinitionSource
 import org.mockito.Mockito._
 import org.scalatest.funspec.AsyncFunSpec
@@ -36,6 +40,15 @@ class DefinitionServiceTest extends AsyncFunSpec with MockitoSugar {
     DefinitionSource.MULTIPLE,
     token = "你好"
   )
+  val dummyGenericDefinition = GenericDefinition(
+    List("definition 1", "definition 2"),
+    "noun",
+    List("example 1", "example 2"),
+    Language.ENGLISH,
+    Language.ENGLISH,
+    DefinitionSource.MULTIPLE,
+    "anything"
+  )
 
   describe("When getting definitions for a single word") {
     it("can get definitions in Chinese") {
@@ -49,6 +62,90 @@ class DefinitionServiceTest extends AsyncFunSpec with MockitoSugar {
           val response = result.get
           assert(response.size == 1)
           assert(response(0) == dummyChineseDefinition)
+        }
+    }
+
+    it("can get definitions in English") {
+      when(mockEnglishService.getDefinitions(Language.CHINESE, "anything"))
+        .thenReturn(Future.successful(Some(List(dummyGenericDefinition))))
+
+      definitionService
+        .getDefinition(Language.ENGLISH, Language.CHINESE, "anything")
+        .map { result =>
+          assert(result.isDefined)
+          val response = result.get
+          assert(response.size == 1)
+          assert(response(0) == dummyGenericDefinition)
+        }
+    }
+
+    it("can get definitions in Spanish") {
+      when(mockSpanishService.getDefinitions(Language.ENGLISH, "cualquier"))
+        .thenReturn(Future.successful(Some(List(dummyGenericDefinition))))
+
+      definitionService
+        .getDefinition(Language.SPANISH, Language.ENGLISH, "cualquier")
+        .map { result =>
+          assert(result.isDefined)
+          val response = result.get
+          assert(response.size == 1)
+          assert(response(0) == dummyGenericDefinition)
+        }
+    }
+  }
+
+  describe("When getting definitions for multiple words") {
+    it("can correctly combine both results") {
+      val secondDummyDefinition = dummyGenericDefinition.copy(token = "another")
+      when(mockEnglishService.getDefinitions(Language.CHINESE, "anything"))
+        .thenReturn(Future.successful(Some(List(dummyGenericDefinition))))
+      when(mockEnglishService.getDefinitions(Language.CHINESE, "another"))
+        .thenReturn(
+          Future.successful(
+            Some(List(dummyGenericDefinition.copy(token = "another")))
+          )
+        )
+
+      definitionService
+        .getDefinitions(
+          Language.ENGLISH,
+          Language.CHINESE,
+          List("anything", "another")
+        )
+        .map { result: Map[String, Option[Seq[Definition]]] =>
+          assert(result.get("anything").isDefined)
+          assert(result.get("another").isDefined)
+
+          val anything = result("anything").get
+          assert(anything.size == 1)
+          assert(anything(0) == dummyGenericDefinition)
+
+          val another = result("another").get
+          assert(another.size == 1)
+          assert(another(0) == secondDummyDefinition)
+        }
+    }
+    it("appropriately handles missing results") {
+      when(mockEnglishService.getDefinitions(Language.CHINESE, "anything"))
+        .thenReturn(Future.successful(Some(List(dummyGenericDefinition))))
+      when(mockEnglishService.getDefinitions(Language.CHINESE, "another"))
+        .thenReturn(Future.successful(None))
+
+      definitionService
+        .getDefinitions(
+          Language.ENGLISH,
+          Language.CHINESE,
+          List("anything", "another")
+        )
+        .map { result =>
+          assert(result.get("anything").isDefined)
+          assert(result.get("another").isDefined)
+
+          val anything = result("anything").get
+          assert(anything.size == 1)
+          assert(anything(0) == dummyGenericDefinition)
+
+          assert(result("another").isEmpty)
         }
     }
   }
