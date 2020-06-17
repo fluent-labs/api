23aeee098d89734901cf5b73706c2dec679013e4
Lucas Kjaero <lucas@lucaskjaero.com>
2020-06-17 00:08:49 -0700

    Test all the things

--- api-v2/test/com/foreignlanguagereader/api/service/definition/CedictTest.scala
+++ api-v2/test/com/foreignlanguagereader/api/service/definition/CedictTest.scala
@@ -20,4 +20,16 @@ class CedictTest extends AnyFunSpec {
       assert(Cedict.convertToTraditional("ABCD") == "ABCD")
     }
   }
+
+  it("can get definitions") {
+    val library = Cedict.getDefinition("圖書館")
+    assert(library.isDefined)
+    val libraryDefinition = library.get
+    assert(libraryDefinition.pinyin == "tu2 shu1 guan3")
+    assert(libraryDefinition.traditional == "圖書館")
+    assert(libraryDefinition.simplified == "图书馆")
+    assert(libraryDefinition.subdefinitions.size == 2)
+    assert(libraryDefinition.subdefinitions(0) == "library")
+    assert(libraryDefinition.subdefinitions(1) == "CL:家[jia1],個|个[ge4]")
+  }
 }
