59b40db4d101e4a74757e532ff8896e349ec484b
Lucas Kjaero <lucas@lucaskjaero.com>
2020-07-14 08:41:23 -0700

    REST clients use the new trait

--- api-v2/app/com/foreignlanguagereader/api/client/LanguageServiceClient.scala
+++ api-v2/app/com/foreignlanguagereader/api/client/LanguageServiceClient.scala
@@ -3,117 +3,59 @@ package com.foreignlanguagereader.api.client
 import java.util.concurrent.TimeUnit
 
 import akka.actor.ActorSystem
+import com.foreignlanguagereader.api.client.common.{
+  CircuitBreakerAttempt,
+  CircuitBreakerResult,
+  Circuitbreaker,
+  WsClient
+}
 import com.foreignlanguagereader.api.contentsource.definition.DefinitionEntry
 import com.foreignlanguagereader.api.domain.Language.Language
-import com.foreignlanguagereader.api.dto.v1.document.WordDTO
-import com.foreignlanguagereader.api.dto.v1.health.ReadinessStatus
-import com.foreignlanguagereader.api.dto.v1.health.ReadinessStatus.ReadinessStatus
+import com.foreignlanguagereader.api.domain.definition.Definition
 import javax.inject.Inject
-import play.api.{Configuration, Logger}
-import play.api.http.Status.OK
-import play.api.libs.json.{JsError, JsSuccess}
+import play.api.libs.json.Reads
 import play.api.libs.ws.WSClient
+import play.api.{Configuration, Logger}
 
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration.{Duration, FiniteDuration}
 import scala.concurrent.{ExecutionContext, Future}
 
 class LanguageServiceClient @Inject()(config: Configuration,
                                       val ws: WSClient,
-                                      val system: ActorSystem) {
-  val logger: Logger = Logger(this.getClass)
-
-  implicit val myExecutionContext: ExecutionContext =
+                                      val system: ActorSystem)
+    extends WsClient
+    with Circuitbreaker {
+  override val logger: Logger = Logger(this.getClass)
+  implicit val ec: ExecutionContext =
     system.dispatchers.lookup("language-service-context")
-
-  val languageServiceBaseUrl: String =
-    config.get[String]("language-service.url")
-  val languageServiceTimeout =
+  override val timeout =
     Duration(config.get[Int]("language-service.timeout"), TimeUnit.SECONDS)
+  override val resetTimeout: FiniteDuration =
+    FiniteDuration(60, TimeUnit.SECONDS)
 
   // This token only works for localhost
   // Will need to replace this with config properties when I learn how secrets work in play
   val languageServiceAuthToken = "simpletoken"
+  override val headers: List[(String, String)] = List(
+    ("Authorization,", languageServiceAuthToken)
+  )
 
-  // Used for the health check. Just makes sure we can connect to language service
-  def checkConnection(timeout: Duration): Future[ReadinessStatus] =
-    ws.url(s"$languageServiceBaseUrl/health")
-      .withRequestTimeout(timeout)
-      .get()
+  val languageServiceBaseUrl: String =
+    config.get[String]("language-service.url")
+
+  implicit val readsDefinitionEntry: Reads[Seq[DefinitionEntry]] =
+    DefinitionEntry.readsSeq
+
+  def getDefinition(
+    wordLanguage: Language,
+    word: String
+  ): Future[CircuitBreakerResult[Option[Seq[Definition]]]] =
+    get(s"$languageServiceBaseUrl/v1/definition/$wordLanguage/$word")
       .map(
-        result =>
-          result.status match {
-            case OK => ReadinessStatus.UP
-            case _ =>
-              logger
-                .error(s"Error connecting to language service: ${result.body}")
-              ReadinessStatus.DOWN
+        results =>
+          results.transform {
+            case Some(r) => Some(r.map(_.toDefinition))
+            case None    => None
         }
       )
-      .recover {
-        case e: Exception =>
-          logger
-            .error(s"Failed to connect to language service: ${e.getMessage}", e)
-          ReadinessStatus.DOWN
-      }
-
-  def getDefinition(wordLanguage: Language,
-                    word: String): Future[Option[Seq[DefinitionEntry]]] = {
-    val url =
-      s"$languageServiceBaseUrl/v1/definition/$wordLanguage/$word"
-    logger.info(s"Calling url $url")
-    ws.url(url)
-      .withRequestTimeout(languageServiceTimeout)
-      .withHttpHeaders(("Authorization", languageServiceAuthToken))
-      .get()
-      .map(response => {
-        response.json.validate[Seq[DefinitionEntry]] match {
-          case JsSuccess(result, _) => Some(result)
-          case JsError(errors) =>
-            logger.error(
-              s"Failed to parse definition in $wordLanguage for word $word from language service: $errors"
-            )
-            throw new IllegalStateException(
-              s"Failed to get definitions in $wordLanguage for word $word"
-            )
-        }
-
-      })
-      .recover {
-        case e: Exception =>
-          logger.error(
-            s"Failed to get definitions in $wordLanguage for word $word from language service: ${e.getMessage}",
-            e
-          )
-          throw e
-      }
-  }
-
-  def getWordsForDocument(documentLanguage: Language,
-                          document: String): Future[Seq[WordDTO]] = {
-    ws.url(s"$languageServiceBaseUrl/v1/tagging/$documentLanguage/document")
-      .withRequestTimeout(languageServiceTimeout)
-      .withHttpHeaders(("Authorization", languageServiceAuthToken))
-      .post(document)
-      .map(
-        response =>
-          response.json.validate[Seq[WordDTO]] match {
-            case JsSuccess(result, _) => result
-            case JsError(errors) =>
-              logger.error(
-                s"Failed to parse words in $documentLanguage for document $document from language service: $errors"
-              )
-              throw new IllegalArgumentException(
-                s"Failed to parse words in $documentLanguage for document $document from language service: $errors"
-              )
-        }
-      )
-      .recover {
-        case e: Exception =>
-          logger.error(
-            s"Failed to get words in $language for document $document from language service: ${e.getMessage}",
-            e
-          )
-          throw e
-      }
-  }
 }
