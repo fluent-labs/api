bb5906369f75390fa08c3b651d00c9c5900c49c1
Lucas Kjaero <lucas@lucaskjaero.com>
2020-07-17 18:05:37 -0700

    Definition entry knows how to handle things

--- api-v2/app/com/foreignlanguagereader/api/contentsource/definition/DefinitionEntry.scala
+++ api-v2/app/com/foreignlanguagereader/api/contentsource/definition/DefinitionEntry.scala
@@ -1,22 +1,46 @@
 package com.foreignlanguagereader.api.contentsource.definition
 
 import com.foreignlanguagereader.api.contentsource.definition.cedict.CEDICTDefinitionEntry
+import com.foreignlanguagereader.api.domain.Language
 import com.foreignlanguagereader.api.domain.Language.Language
 import com.foreignlanguagereader.api.domain.definition.DefinitionSource.DefinitionSource
 import com.foreignlanguagereader.api.domain.definition.{
+  ChineseDefinition,
   Definition,
   DefinitionSource
 }
+import com.foreignlanguagereader.api.domain.word.PartOfSpeech.PartOfSpeech
 import play.api.libs.json._
 
 trait DefinitionEntry {
   val subdefinitions: List[String]
+  val pronunciation: String
+  val tag: Option[PartOfSpeech]
+  val examples: Option[List[String]]
+
   val wordLanguage: Language
   val definitionLanguage: Language
   val source: DefinitionSource
   val token: String
 
-  val toDefinition: Definition
+  lazy val toDefinition: Definition = {
+    wordLanguage match {
+      case Language.CHINESE => DefinitionEntry.buildChineseDefinition(this)
+      case Language.CHINESE_TRADITIONAL =>
+        DefinitionEntry.buildChineseDefinition(this)
+      case _ =>
+        Definition(
+          subdefinitions = subdefinitions,
+          ipa = pronunciation,
+          tag = tag,
+          examples = examples,
+          wordLanguage = wordLanguage,
+          definitionLanguage = definitionLanguage,
+          source = source,
+          token = token
+        )
+    }
+  }
 }
 
 object DefinitionEntry {
@@ -51,4 +75,18 @@ object DefinitionEntry {
 
   def convertToSeqOfDefinitions(d: Seq[DefinitionEntry]): Seq[Definition] =
     d.map(e => e.toDefinition)
+
+  def buildChineseDefinition(entry: DefinitionEntry): ChineseDefinition =
+    ChineseDefinition(
+      subdefinitions = entry.subdefinitions,
+      tag = entry.tag,
+      examples = entry.examples,
+      inputPinyin = entry.pronunciation,
+      isTraditional = entry.wordLanguage == Language.CHINESE_TRADITIONAL,
+      inputSimplified = None,
+      inputTraditional = None,
+      definitionLanguage = entry.definitionLanguage,
+      source = entry.source,
+      token = entry.token
+    )
 }
