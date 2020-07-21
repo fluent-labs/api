2b19bb36201bd1fd4897c443b2e60dd86df0321c
Lucas Kjaero <lucas@lucaskjaero.com>
2020-07-20 23:10:25 -0700

    Test update vs insert for cached results

--- api-v2/test/com/foreignlanguagereader/api/client/elasticsearch/searchstates/ElasticsearchResultTest.scala
+++ api-v2/test/com/foreignlanguagereader/api/client/elasticsearch/searchstates/ElasticsearchResultTest.scala
@@ -9,7 +9,7 @@ import com.foreignlanguagereader.api.domain.definition.{
   GenericDefinition
 }
 import com.foreignlanguagereader.api.domain.word.PartOfSpeech
-import com.sksamuel.elastic4s.ElasticDsl.indexInto
+import com.sksamuel.elastic4s.ElasticDsl.{indexInto, updateById}
 import com.sksamuel.elastic4s.playjson._
 import org.scalatest.funspec.AnyFunSpec
 
@@ -34,6 +34,7 @@ class ElasticsearchResultTest extends AnyFunSpec {
         assert(result.toIndex.isEmpty)
       }
     }
+
     describe("where refetching gave no result") {
       val result = ElasticsearchResult[Definition](
         index = index,
@@ -53,43 +54,75 @@ class ElasticsearchResultTest extends AnyFunSpec {
         assert(result.toIndex.contains(List(attemptsQuery)))
       }
     }
-    describe("where refetching produced results") {
-      val dummyChineseDefinition = ChineseDefinition(
-        subdefinitions = List("definition 1", "definition 2"),
-        tag = Some(PartOfSpeech.NOUN),
-        examples = Some(List("example 1", "example 2")),
-        inputPinyin = "ni3 hao3",
-        inputSimplified = Some("你好"),
-        inputTraditional = Some("你好"),
-        definitionLanguage = Language.ENGLISH,
-        source = DefinitionSource.MULTIPLE,
-        token = "你好"
-      )
-      val dummyGenericDefinition = GenericDefinition(
-        subdefinitions = List("definition 1", "definition 2"),
-        ipa = "ipa",
-        tag = Some(PartOfSpeech.NOUN),
-        examples = Some(List("example 1", "example 2")),
-        definitionLanguage = Language.ENGLISH,
-        wordLanguage = Language.ENGLISH,
-        source = DefinitionSource.MULTIPLE,
-        token = "anything"
-      )
+
+    val dummyChineseDefinition = ChineseDefinition(
+      subdefinitions = List("definition 1", "definition 2"),
+      tag = Some(PartOfSpeech.NOUN),
+      examples = Some(List("example 1", "example 2")),
+      inputPinyin = "ni3 hao3",
+      inputSimplified = Some("你好"),
+      inputTraditional = Some("你好"),
+      definitionLanguage = Language.ENGLISH,
+      source = DefinitionSource.MULTIPLE,
+      token = "你好"
+    )
+    val dummyGenericDefinition = GenericDefinition(
+      subdefinitions = List("definition 1", "definition 2"),
+      ipa = "ipa",
+      tag = Some(PartOfSpeech.NOUN),
+      examples = Some(List("example 1", "example 2")),
+      definitionLanguage = Language.ENGLISH,
+      wordLanguage = Language.ENGLISH,
+      source = DefinitionSource.MULTIPLE,
+      token = "anything"
+    )
+
+    describe("on a previously untried query") {
       val result = ElasticsearchResult[Definition](
         index = index,
         fields = fields,
         result = Some(List(dummyChineseDefinition, dummyGenericDefinition)),
-        fetchCount = 5,
+        fetchCount = 1,
         lookupId = None,
         refetched = true
       )
       val attemptsQuery =
-        Left(indexInto("attempts").doc(LookupAttempt(index, fields, 5)))
+        Left(indexInto("attempts").doc(LookupAttempt(index, fields, 1)))
       val indexQuery = List(
         Left(indexInto(index).doc(dummyChineseDefinition)),
         Left(indexInto(index).doc(dummyGenericDefinition))
       )
-      it("correctly saves fetch attempts to elasticsearch") {
+      it("creates a new fetch attempt in elasticsearch") {
+        assert(result.updateAttemptsQuery.contains(attemptsQuery))
+      }
+      it("caches search results to elasticsearch") {
+        assert(result.cacheQueries.contains(indexQuery))
+        assert(result.toIndex.contains(attemptsQuery :: indexQuery))
+      }
+    }
+
+    describe(
+      "on a query which previously failed but contained results this time"
+    ) {
+      val attemptsId = "2423423"
+      val result = ElasticsearchResult[Definition](
+        index = index,
+        fields = fields,
+        result = Some(List(dummyChineseDefinition, dummyGenericDefinition)),
+        fetchCount = 2,
+        lookupId = Some(attemptsId),
+        refetched = true
+      )
+      val attemptsQuery =
+        Right(
+          updateById("attempts", attemptsId)
+            .doc(LookupAttempt(index = index, fields = fields, count = 2))
+        )
+      val indexQuery = List(
+        Left(indexInto(index).doc(dummyChineseDefinition)),
+        Left(indexInto(index).doc(dummyGenericDefinition))
+      )
+      it("updates the previous fetch attempt in elasticsearch") {
         assert(result.updateAttemptsQuery.contains(attemptsQuery))
       }
       it("caches search results to elasticsearch") {
