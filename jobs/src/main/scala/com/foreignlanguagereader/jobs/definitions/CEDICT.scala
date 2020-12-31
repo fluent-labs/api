a87b42c5d4becf092b21d5b975cea6519fe68d9f
Lucas Kjaero <lucas@lucaskjaero.com>
2020-12-30 21:43:46 -0800

    Base CEDICT parsing logic

---
+++ jobs/src/main/scala/com/foreignlanguagereader/jobs/definitions/CEDICT.scala
@@ -0,0 +1,36 @@
+package com.foreignlanguagereader.jobs.definitions
+
+import com.foreignlanguagereader.content.types.external.definition.cedict.CEDICTDefinitionEntry
+import com.foreignlanguagereader.content.types.internal.definition.DefinitionSource
+import org.apache.spark.sql.{Dataset, SparkSession}
+
+import scala.util.matching.Regex
+
+object CEDICT
+    extends DefinitionsParsingJob[CEDICTDefinitionEntry](
+      "s3a://foreign-language-reader-content/definitions/cedict/",
+      DefinitionSource.CEDICT
+    ) {
+  val lineRegex: Regex = "/([^ ]+)\\s([^ ]+) \\[(.*)\\] \\/(.*)\\/\\n/g".r
+
+  override def loadFromPath(path: String)(implicit
+      spark: SparkSession
+  ): Dataset[CEDICTDefinitionEntry] = {
+    import spark.implicits._
+    spark.read
+      .textFile(path)
+      // These lines are license and parsing instructions
+      .filter(line => line.startsWith("#"))
+      .map(line => {
+        val lineRegex(traditional, simplified, pinyin, definitions) = line
+        val subdefinitions = definitions.split("/")
+        CEDICTDefinitionEntry(
+          subdefinitions = subdefinitions.toList,
+          pinyin = pinyin,
+          simplified = simplified,
+          traditional = traditional,
+          token = traditional
+        )
+      })
+  }
+}
