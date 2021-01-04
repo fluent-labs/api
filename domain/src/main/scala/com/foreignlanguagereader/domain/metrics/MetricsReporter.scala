ff30d050f48e295f421d4d5d9404c6408c4bd377
Lucas Kjaero <lucas@lucaskjaero.com>
2021-01-03 17:34:40 -0800

    Separate labeled and unlabeled counters

--- domain/src/main/scala/com/foreignlanguagereader/domain/metrics/MetricsReporter.scala
+++ domain/src/main/scala/com/foreignlanguagereader/domain/metrics/MetricsReporter.scala
@@ -1,29 +1,42 @@
 package com.foreignlanguagereader.domain.metrics
 
 import com.foreignlanguagereader.domain.metrics.Metric.Metric
-import io.prometheus.client.{CollectorRegistry, Counter, Gauge, Histogram}
+import io.prometheus.client.{Counter, Gauge, Histogram}
 
 import java.util.concurrent.Callable
 import javax.inject.Singleton
 
 @Singleton
 class MetricsReporter {
-  def buildCounter(
-      metric: Metric,
-      labelNames: String
-  ): Counter =
+  def makeCounterBuilder(metric: Metric): Counter.Builder = {
     Counter
       .build()
       .name(metric.toString.toLowerCase)
       .help(metric.toString.toLowerCase.replaceAll("_", " "))
+  }
+
+  def buildCounter(
+      metric: Metric
+  ): Counter =
+    makeCounterBuilder(metric)
+      .register()
+
+  def buildCounter(
+      metric: Metric,
+      labelNames: String
+  ): Counter =
+    makeCounterBuilder(metric)
       .labelNames(labelNames)
       .register()
 
-  val counters: Map[Metric, Counter] = Map(
+  val unlabeledCounters: Map[Metric, Counter] =
+    List(Metric.GOOGLE_SUCCESSES, Metric.GOOGLE_FAILURES)
+      .map(metric => metric -> buildCounter(metric))
+      .toMap
+
+  val labeledCounters: Map[Metric, Counter] = Map(
     Metric.ELASTICSEARCH_SUCCESSES -> "method",
     Metric.ELASTICSEARCH_FAILURES -> "method",
-    Metric.GOOGLE_SUCCESSES -> "api",
-    Metric.GOOGLE_FAILURES -> "api",
     Metric.WEBSTER_SUCCESSES -> "dictionary",
     Metric.WEBSTER_FAILURES -> "dictionary",
     Metric.REQUEST_SUCCESSES -> "route",
@@ -33,9 +46,10 @@ class MetricsReporter {
     case (metric, labels) => metric -> buildCounter(metric, labels)
   }
 
-  def report(metric: Metric): Unit = counters.get(metric).foreach(_.inc())
+  def report(metric: Metric): Unit =
+    unlabeledCounters.get(metric).foreach(_.inc())
   def report(metric: Metric, label: String): Unit =
-    counters.get(metric).foreach(_.labels(label).inc())
+    labeledCounters.get(metric).foreach(_.labels(label).inc())
 
   def buildGauge(
       metric: Metric,
@@ -45,7 +59,6 @@ class MetricsReporter {
       .build()
       .name(metric.toString.toLowerCase)
       .help(metric.toString.toLowerCase.replaceAll("_", " "))
-      .labelNames(labelNames)
       .register()
 
   val gauges: Map[Metric, Gauge] = Map(Metric.ACTIVE_REQUESTS -> "route").map {
