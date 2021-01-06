2264d39357e637aa988fc1a69a25d13a421ea8d1
Lucas Kjaero <lucas@lucaskjaero.com>
2021-01-05 17:59:49 -0800

    Fix insecure regex

--- api/app/com/foreignlanguagereader/api/metrics/ApiMetricReporter.scala
+++ api/app/com/foreignlanguagereader/api/metrics/ApiMetricReporter.scala
@@ -1,9 +1,13 @@
 package com.foreignlanguagereader.api.metrics
 
+import com.foreignlanguagereader.content.types.Language
+
 import scala.util.matching.Regex
 
 object ApiMetricReporter {
-  val definitionsRegex: Regex = "/v1/language/definition/[A-Z]+/[^/]+[/]+".r
+  val languageRegex: String = s"[${Language.values.mkString("|")}]+"
+  val definitionsRegex: Regex =
+    "/v1/language/definition/$languageRegex/[^/]+/?".r
 
   def getLabelFromPath(path: String): String =
     path match {
