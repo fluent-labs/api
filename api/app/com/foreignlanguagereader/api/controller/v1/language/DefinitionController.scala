abae5a1d966da90a1ccba9f425f974ac1d7efc8c
Lucas Kjaero <lucas@lucaskjaero.com>
2021-01-05 17:46:24 -0800

    Wire everything in

--- api/app/com/foreignlanguagereader/api/controller/v1/language/DefinitionController.scala
+++ api/app/com/foreignlanguagereader/api/controller/v1/language/DefinitionController.scala
@@ -3,7 +3,7 @@ package com.foreignlanguagereader.api.controller.v1.language
 import com.foreignlanguagereader.content.types.Language
 import com.foreignlanguagereader.content.types.Language.Language
 import com.foreignlanguagereader.content.types.internal.word.Word
-import com.foreignlanguagereader.domain.metrics.{Metric, MetricsReporter}
+import com.foreignlanguagereader.domain.metrics.MetricsReporter
 import com.foreignlanguagereader.domain.service.definition.DefinitionService
 import play.api.Logger
 import play.api.mvc._
@@ -20,7 +20,6 @@ class DefinitionController @Inject() (
     implicit val ec: ExecutionContext
 ) extends BaseController {
   val logger: Logger = Logger(this.getClass)
-  val definitionLabel = "definitions"
 
   def definition(wordLanguage: Language, word: String): Action[AnyContent] =
     Action.async {
@@ -32,44 +31,26 @@ class DefinitionController @Inject() (
       definitionLanguage: Language,
       word: String
   ): Future[Result] = {
-    metrics.timeRequest(definitionLabel)(() => {
-      metrics.inc(Metric.ACTIVE_REQUESTS)
-      metrics.report(Metric.REQUEST_COUNT, definitionLabel)
-      metrics.reportLanguageUsage(wordLanguage, definitionLanguage)
-      logger.info(
-        s"Getting definitions in $definitionLanguage for $wordLanguage word $word"
-      )
+    metrics.reportLanguageUsage(wordLanguage, definitionLanguage)
+    logger.info(
+      s"Getting definitions in $definitionLanguage for $wordLanguage word $word"
+    )
 
-      definitionService
-        .getDefinition(
-          wordLanguage,
-          definitionLanguage,
-          Word.fromToken(word, wordLanguage)
-        )
-        .map { definitions =>
-          {
-            Ok(
-              JavaJson.stringify(
-                JavaJson
-                  .toJson(definitions.map(_.toDTO))
-              )
+    definitionService
+      .getDefinition(
+        wordLanguage,
+        definitionLanguage,
+        Word.fromToken(word, wordLanguage)
+      )
+      .map { definitions =>
+        {
+          Ok(
+            JavaJson.stringify(
+              JavaJson
+                .toJson(definitions.map(_.toDTO))
             )
-          }
+          )
         }
-        .recover {
-          case error: Throwable =>
-            metrics.report(Metric.REQUEST_FAILURES, definitionLabel)
-            logger.error(
-              s"Failed to get definitions for $word in $wordLanguage: ${error.getMessage}",
-              error
-            )
-            InternalServerError(
-              s"Failed to get definitions for $word in $wordLanguage"
-            )
-        }
-    })
-  }.map(r => {
-    metrics.dec(Metric.ACTIVE_REQUESTS)
-    r
-  })
+      }
+  }
 }
--- api/app/com/foreignlanguagereader/api/controller/v1/language/DocumentController.scala
+++ api/app/com/foreignlanguagereader/api/controller/v1/language/DocumentController.scala
@@ -4,13 +4,12 @@ import com.foreignlanguagereader.content.types.Language.Language
 import com.foreignlanguagereader.domain.metrics.{Metric, MetricsReporter}
 import com.foreignlanguagereader.domain.service.DocumentService
 import com.foreignlanguagereader.dto.v1.document.DocumentRequest
-
-import javax.inject._
 import play.api.Logger
 import play.api.libs.json._
 import play.api.mvc._
 import play.libs.{Json => JavaJson}
 
+import javax.inject._
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
@@ -32,43 +31,28 @@ class DocumentController @Inject() (
       definitionLanguage: Language
   ): Action[JsValue] =
     Action.async(parse.json) { request =>
-      metrics
-        .timeRequest(documentLabel)(() => {
-          metrics.inc(Metric.ACTIVE_REQUESTS)
-          metrics.report(Metric.REQUEST_COUNT, documentLabel)
-          metrics.reportLanguageUsage(wordLanguage, definitionLanguage)
-          request.body.validate[DocumentRequest] match {
-            case JsSuccess(documentRequest: DocumentRequest, _) =>
-              documentService
-                .getWordsForDocument(
-                  wordLanguage,
-                  definitionLanguage,
-                  documentRequest.getText
-                )
-                .map(words => {
-                  Ok(JavaJson.stringify(JavaJson.toJson(words.map(_.toDTO))))
-                })
-                .recover {
-                  case error: Throwable =>
-                    val message =
-                      s"Failed to get words in $wordLanguage: ${error.getMessage}"
-                    logger.error(message, error)
-                    metrics.report(Metric.REQUEST_FAILURES, documentLabel)
-                    InternalServerError(message)
-                }
-            case JsError(errors) =>
-              logger.error(
-                s"Invalid request body given to document service: $errors"
+      {
+        metrics.reportLanguageUsage(wordLanguage, definitionLanguage)
+        request.body.validate[DocumentRequest] match {
+          case JsSuccess(documentRequest: DocumentRequest, _) =>
+            documentService
+              .getWordsForDocument(
+                wordLanguage,
+                definitionLanguage,
+                documentRequest.getText
               )
-              metrics.report(Metric.BAD_REQUEST_DATA, documentLabel)
-              Future {
-                BadRequest("Invalid request body, please try again")
-              }
-          }
-        })
-        .map(r => {
-          metrics.dec(Metric.ACTIVE_REQUESTS)
-          r
-        })
+              .map(words => {
+                Ok(JavaJson.stringify(JavaJson.toJson(words.map(_.toDTO))))
+              })
+          case JsError(errors) =>
+            logger.error(
+              s"Invalid request body given to document service: $errors"
+            )
+            metrics.report(Metric.BAD_REQUEST_DATA, documentLabel)
+            Future {
+              BadRequest("Invalid request body, please try again")
+            }
+        }
+      }
     }
 }
