bc355300210f55e9da191fd42fb55249325d679f
Lucas Kjaero <lucas@lucaskjaero.com>
2021-01-09 09:49:42 -0800

    DAO for word

--- domain/src/main/scala/com/foreignlanguagereader/domain/repository/WordRepository.scala
+++ domain/src/main/scala/com/foreignlanguagereader/domain/repository/WordRepository.scala
@@ -8,40 +8,44 @@ import com.foreignlanguagereader.content.types.internal.word.WordTense.WordTense
 import com.foreignlanguagereader.domain.repository.EnumImplicits._
 import slick.jdbc.H2Profile.api._
 
-/**
-  * language: Language,
-  * token: String,
-  * tag: PartOfSpeech,
-  * lemma: String,
-  * definitions: Seq[Definition],
-  * gender: Option[GrammaticalGender],
-  * number: Option[Count],
-  * proper: Option[Boolean],
-  * tense: Option[WordTense],
-  * processedToken: String
-  *
-  * @param tag
-  */
-
 class WordDAO(tag: Tag)
-    extends Table[(String, Int, Double, Int, Int)](tag, "COFFEES") {
+    extends Table[
+      (
+          Language,
+          String,
+          PartOfSpeech,
+          String,
+          Option[GrammaticalGender],
+          Option[Count],
+          Boolean,
+          Option[WordTense],
+          String
+      )
+    ](tag, "Word") {
   def language: Rep[Language] = column[Language]("language")
   def token: Rep[String] = column[String]("token")
-  def tag: Rep[PartOfSpeech] = column[PartOfSpeech]("tag")
+  def wordTag: Rep[PartOfSpeech] = column[PartOfSpeech]("tag")
   def lemma: Rep[String] = column[String]("lemma")
   // definitions
-  def gender = column[Option[GrammaticalGender]]("gender")
-  def number = column[Option[Count]]("count")
-  def proper = column[Boolean]("proper")
-  def tense = column[Option[WordTense]]("tense")
+  def gender: Rep[Option[GrammaticalGender]] =
+    column[Option[GrammaticalGender]]("gender")
+  def number: Rep[Option[Count]] = column[Option[Count]]("count")
+  def proper: Rep[Boolean] = column[Boolean]("proper")
+  def tense: Rep[Option[WordTense]] = column[Option[WordTense]]("tense")
   def processedToken: Rep[String] = column[String]("processedtoken")
 
-  def name = column[String]("COF_NAME", O.PrimaryKey)
-  def supID = column[Int]("SUP_ID")
-  def price = column[Double]("PRICE")
-  def sales = column[Int]("SALES", O.Default(0))
-  def total = column[Int]("TOTAL", O.Default(0))
-  def * = (name, supID, price, sales, total)
+  def * =
+    (
+      language,
+      token,
+      wordTag,
+      lemma,
+      gender,
+      number,
+      proper,
+      tense,
+      processedToken
+    )
 }
 
 class WordRepository {}
