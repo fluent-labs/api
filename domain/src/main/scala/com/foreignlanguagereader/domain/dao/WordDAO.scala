89e9b73db8c5a3db14615ac66db5bed29103829b
Lucas Kjaero <lucas@lucaskjaero.com>
2021-01-09 14:34:21 -0800

    Proper word DAO

---
+++ domain/src/main/scala/com/foreignlanguagereader/domain/dao/WordDAO.scala
@@ -0,0 +1,49 @@
+package com.foreignlanguagereader.domain.dao
+
+import com.foreignlanguagereader.content.types.Language.Language
+import com.foreignlanguagereader.content.types.internal.word.Count.Count
+import com.foreignlanguagereader.content.types.internal.word.GrammaticalGender.GrammaticalGender
+import com.foreignlanguagereader.content.types.internal.word.PartOfSpeech.PartOfSpeech
+import com.foreignlanguagereader.content.types.internal.word.WordTense.WordTense
+import com.foreignlanguagereader.domain.repository.EnumImplicits._
+import slick.jdbc.H2Profile.api._
+
+class WordDAO(tag: Tag)
+    extends Table[
+      (
+          Language,
+          String,
+          PartOfSpeech,
+          String,
+          Option[GrammaticalGender],
+          Option[Count],
+          Boolean,
+          Option[WordTense],
+          String
+      )
+    ](tag, "Word") {
+  def language: Rep[Language] = column[Language]("language")
+  def token: Rep[String] = column[String]("token")
+  def wordTag: Rep[PartOfSpeech] = column[PartOfSpeech]("tag")
+  def lemma: Rep[String] = column[String]("lemma")
+  // definitions
+  def gender: Rep[Option[GrammaticalGender]] =
+    column[Option[GrammaticalGender]]("gender")
+  def number: Rep[Option[Count]] = column[Option[Count]]("count")
+  def proper: Rep[Boolean] = column[Boolean]("proper")
+  def tense: Rep[Option[WordTense]] = column[Option[WordTense]]("tense")
+  def processedToken: Rep[String] = column[String]("processedtoken")
+
+  def * =
+    (
+      language,
+      token,
+      wordTag,
+      lemma,
+      gender,
+      number,
+      proper,
+      tense,
+      processedToken
+    )
+}
