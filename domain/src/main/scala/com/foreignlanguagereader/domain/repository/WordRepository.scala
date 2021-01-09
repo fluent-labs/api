346f9e8c2c4bbee8b2269fcc06db63f8f7f4125e
Lucas Kjaero <lucas@lucaskjaero.com>
2021-01-09 14:36:39 -0800

    Setup code in the repository

--- domain/src/main/scala/com/foreignlanguagereader/domain/repository/WordRepository.scala
+++ domain/src/main/scala/com/foreignlanguagereader/domain/repository/WordRepository.scala
@@ -1,51 +1,20 @@
 package com.foreignlanguagereader.domain.repository
 
-import com.foreignlanguagereader.content.types.Language.Language
-import com.foreignlanguagereader.content.types.internal.word.Count.Count
-import com.foreignlanguagereader.content.types.internal.word.GrammaticalGender.GrammaticalGender
-import com.foreignlanguagereader.content.types.internal.word.PartOfSpeech.PartOfSpeech
-import com.foreignlanguagereader.content.types.internal.word.WordTense.WordTense
-import com.foreignlanguagereader.domain.repository.EnumImplicits._
+import com.foreignlanguagereader.content.types.Language
+import com.foreignlanguagereader.content.types.internal.word.PartOfSpeech
+import com.foreignlanguagereader.domain.client.database.DatabaseConnectionHolder
+import com.foreignlanguagereader.domain.dao.WordDAO
 import slick.jdbc.H2Profile.api._
 
-class WordDAO(tag: Tag)
-    extends Table[
-      (
-          Language,
-          String,
-          PartOfSpeech,
-          String,
-          Option[GrammaticalGender],
-          Option[Count],
-          Boolean,
-          Option[WordTense],
-          String
-      )
-    ](tag, "Word") {
-  def language: Rep[Language] = column[Language]("language")
-  def token: Rep[String] = column[String]("token")
-  def wordTag: Rep[PartOfSpeech] = column[PartOfSpeech]("tag")
-  def lemma: Rep[String] = column[String]("lemma")
-  // definitions
-  def gender: Rep[Option[GrammaticalGender]] =
-    column[Option[GrammaticalGender]]("gender")
-  def number: Rep[Option[Count]] = column[Option[Count]]("count")
-  def proper: Rep[Boolean] = column[Boolean]("proper")
-  def tense: Rep[Option[WordTense]] = column[Option[WordTense]]("tense")
-  def processedToken: Rep[String] = column[String]("processedtoken")
+import javax.inject.Inject
+import scala.concurrent.Future
 
-  def * =
-    (
-      language,
-      token,
-      wordTag,
-      lemma,
-      gender,
-      number,
-      proper,
-      tense,
-      processedToken
-    )
+class WordRepository @Inject() (db: DatabaseConnectionHolder) {
+  val word = TableQuery[WordDAO]
+  val setup = DBIO.seq(
+    word.schema.createIfNotExists,
+    word += (Language.ENGLISH, "test", PartOfSpeech.NOUN, "test", None, None, false, None, "test")
+  )
+
+  val setupFuture: Future[Unit] = db.run(setup)
 }
-
-class WordRepository {}
