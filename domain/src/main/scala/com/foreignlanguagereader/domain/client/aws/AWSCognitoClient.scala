3bff82e8da2f457d499f79e35284124164fbe680
Lucas Kjaero <lucas@lucaskjaero.com>
2021-01-17 08:16:14 -0800

    Signup flow

--- domain/src/main/scala/com/foreignlanguagereader/domain/client/aws/AWSCognitoClient.scala
+++ domain/src/main/scala/com/foreignlanguagereader/domain/client/aws/AWSCognitoClient.scala
@@ -1,14 +1,30 @@
 package com.foreignlanguagereader.domain.client.aws
 
 import akka.actor.ActorSystem
-import com.foreignlanguagereader.domain.client.common.Circuitbreaker
+import com.foreignlanguagereader.domain.client.common.{
+  CircuitBreakerResult,
+  Circuitbreaker
+}
 import com.foreignlanguagereader.domain.metrics.MetricsReporter
+import com.foreignlanguagereader.domain.metrics.label.CognitoRequestType
 import play.api.Logger
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient
+import software.amazon.awssdk.services.cognitoidentityprovider.model.{
+  AttributeType,
+  AuthFlowType,
+  InitiateAuthRequest,
+  InitiateAuthResponse,
+  SignUpRequest,
+  SignUpResponse
+}
 
+import java.nio.charset.StandardCharsets
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
 import javax.inject.{Inject, Singleton}
-import scala.concurrent.ExecutionContext
+import scala.collection.JavaConverters._
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class AWSCognitoClient @Inject() (
@@ -16,6 +32,14 @@ class AWSCognitoClient @Inject() (
     implicit val ec: ExecutionContext,
     val system: ActorSystem
 ) {
+  // Secret, generate in AWS SDK
+  val clientId = ""
+  val secretKey = ""
+
+  val HMAC_SHA256_ALGORITHM = "HmacSHA256"
+
+  val logger: Logger = Logger(this.getClass)
+  val breaker: Circuitbreaker = new Circuitbreaker(system, ec, "Cognito")
   val identityProviderClient: CognitoIdentityProviderClient =
     CognitoIdentityProviderClient
       .builder()
@@ -24,4 +48,54 @@ class AWSCognitoClient @Inject() (
 
   val logger: Logger = Logger(this.getClass)
   val breaker: Circuitbreaker = new Circuitbreaker(system, ec, "Cognito")
+
+  def signup(
+      email: String,
+      password: String
+  ): Future[CircuitBreakerResult[SignUpResponse]] = {
+    val timer = metrics.reportCognitoRequestStarted(CognitoRequestType.SIGNUP)
+    breaker.withBreaker(e => {
+      metrics.reportCognitoFailure(timer, CognitoRequestType.SIGNUP)
+      logger.error(s"Failed to create user $email: ${e.getMessage}", e)
+    }) {
+      Future {
+        val result =
+          identityProviderClient.signUp(makeSignupRequest(email, password))
+        metrics.reportCognitoRequestFinished(timer)
+        result
+      }
+    }
+  }
+
+  private[this] def makeSignupRequest(email: String, password: String) = {
+    val attributes = List(
+      AttributeType
+        .builder()
+        .name("email")
+        .value(email)
+        .build()
+    ).asJava
+    SignUpRequest
+      .builder()
+      .userAttributes(attributes)
+      .username(email)
+      .clientId(clientId)
+      .password(password)
+      .secretHash(calculateHash(email))
+      .build()
+  }
+
+  private[this] def calculateHash(username: String): String = {
+    val mac = Mac.getInstance(HMAC_SHA256_ALGORITHM)
+    mac.init(
+      new SecretKeySpec(
+        secretKey.getBytes(StandardCharsets.UTF_8),
+        HMAC_SHA256_ALGORITHM
+      )
+    )
+
+    mac.update(username.getBytes(StandardCharsets.UTF_8))
+    val rawHmac = mac.doFinal(clientId.getBytes(StandardCharsets.UTF_8))
+    java.util.Base64.getEncoder.encodeToString(rawHmac)
+  }
 }
