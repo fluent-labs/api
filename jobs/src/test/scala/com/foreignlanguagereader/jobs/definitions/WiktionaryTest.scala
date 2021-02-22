3503a823bdc331503f440d28a34cd4a28f02d7eb
Lucas Kjaero <lucas@lucaskjaero.com>
2021-02-21 23:16:27 -0800

    Test for getting headings

--- jobs/src/test/scala/com/foreignlanguagereader/jobs/definitions/WiktionaryTest.scala
+++ jobs/src/test/scala/com/foreignlanguagereader/jobs/definitions/WiktionaryTest.scala
@@ -3,44 +3,53 @@ package com.foreignlanguagereader.jobs.definitions
 import org.scalatest.funspec.AnyFunSpec
 
 class WiktionaryTest extends AnyFunSpec {
-  describe("a wiktionary test") {
-    describe("can correctly generate regexes") {
-      it("can repeat a pattern") {
-        assert(Wiktionary.repeat("=", 6) == "======")
+  describe("can correctly generate regexes") {
+    it("can repeat a pattern") {
+      assert(Wiktionary.repeat("=", 6) == "======")
+    }
+
+    describe("for a heading of any size") {
+      val levelThreeHeading = Wiktionary.headingRegex(3)
+      it("which match valid headings") {
+        assert("=== Title ===".matches(levelThreeHeading))
       }
 
-      describe("for a heading of any size") {
-        val levelThreeHeading = Wiktionary.headingRegex(3)
-        it("which match valid headings") {
-          assert("=== Title ===".matches(levelThreeHeading))
-        }
+      // There's some subtle bugs around matching too many and too few
+      // This is to prevent regression
 
-        // There's some subtle bugs around matching too many and too few
-        // This is to prevent regression
-
-        it("does not match larger headings") {
-          assert(!"== Title ==".matches(levelThreeHeading))
-        }
-
-        it("does not match smaller headings") {
-          assert(!"==== Title ====".matches(levelThreeHeading))
-        }
+      it("does not match larger headings") {
+        assert(!"== Title ==".matches(levelThreeHeading))
       }
 
-      // This is there to cover refactors, feel free to wipe the assertion if the regex materially changes.
-      it("for a section") {
-        assert(
-          "(?s)(?i)== *MyTestSection *==(.*?)(?>(?>== *[A-Za-z0-9]+ *==[ |\n]+)|\\Z)+" == Wiktionary
-            .sectionRegex("MyTestSection")
-        )
+      it("does not match smaller headings") {
+        assert(!"==== Title ====".matches(levelThreeHeading))
       }
+    }
 
-      // This is there to cover refactors, feel free to wipe the assertion if the regex materially changes.
-      it("for a subsection") {
-        assert(
-          "(?s)(?i)=== *MyTestSubsection *===(.*?)(?>(?>== *[A-Za-z0-9]+ *==[ |\n]+)|\\Z)+" == Wiktionary
-            .subSectionRegex("MyTestSubsection")
-        )
+    // This is there to cover refactors, feel free to wipe the assertion if the regex materially changes.
+    it("for a section") {
+      assert(
+        "(?s)(?i)== *MyTestSection *==(.*?)(?>(?>== *[A-Za-z0-9]+ *==[ |\n]+)|\\Z)+" == Wiktionary
+          .sectionRegex("MyTestSection")
+      )
+    }
+
+    // This is there to cover refactors, feel free to wipe the assertion if the regex materially changes.
+    it("for a subsection") {
+      assert(
+        "(?s)(?i)=== *MyTestSubsection *===(.*?)(?>(?>== *[A-Za-z0-9]+ *==[ |\n]+)|\\Z)+" == Wiktionary
+          .subSectionRegex("MyTestSubsection")
+      )
+    }
+  }
+  describe("can get headings") {
+    describe("for a single line") {
+      it("on the happy path") {
+        val test = "== Single heading =="
+        assert(Wiktionary.getHeadingFromLine(test, 2) == "single heading")
+      }
+      it("without error if there is no heading") {
+        assert(Wiktionary.getHeadingFromLine("", 2) == "")
       }
     }
   }
