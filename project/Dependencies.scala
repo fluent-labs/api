97448b1e90f8c7fd1cdbf71001f8589b60c5423f
Lucas Kjaero-Zhang <lucaskjaero@users.noreply.github.com>
2021-09-09 09:35:10 +0200

    Update opencc4j to 1.7.1 (#416)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -46,7 +46,7 @@ object Dependencies {
     "io.prometheus" % "simpleclient_hotspot" % prometheusVersion
 
   // NLP tools
-  val opencc4j = "com.github.houbb" % "opencc4j" % "1.6.2"
+  val opencc4j = "com.github.houbb" % "opencc4j" % "1.7.1"
 
   // External clients
   val elasticsearchHighLevelClient =
