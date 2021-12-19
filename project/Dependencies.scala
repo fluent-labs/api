e7bb21f537b9f194aef3277571de13031b6c4454
Lucas Kjaero <lucas@lucaskjaero.com>
2021-12-19 15:26:45 +0100

    Use a 2.13 upgraded content version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   val scalatestVersion = "3.2.10"
   val sparkVersion = "3.1.2"
 
-  val content = "io.fluentlabs" %% "content" % "1.0.1"
+  val content = "io.fluentlabs" %% "content" % "1.0.12"
 
   // Testing
   val scalactic = "org.scalactic" %% "scalactic" % scalatestVersion
