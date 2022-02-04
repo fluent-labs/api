d39d3b2c826df10527e2e03f6346121894430039
Lucas Kjaero-Zhang <lucas@lucaskjaero.com>
2022-02-04 00:44:19 +0000

    Update google-cloud-language to 2.1.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -53,7 +53,7 @@ object Dependencies {
     "org.elasticsearch.client" % "elasticsearch-rest-high-level-client" % elasticsearchVersion
   val oslib = "com.lihaoyi" %% "os-lib" % "0.8.0"
   val googleCloudClient =
-    "com.google.cloud" % "google-cloud-language" % "2.1.5"
+    "com.google.cloud" % "google-cloud-language" % "2.1.6"
 
   // Database
   val h2 = "com.h2database" % "h2" % "2.1.210"
